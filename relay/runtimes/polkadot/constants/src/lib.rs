c8c92ecf9e5dac45ec956151a89b5af2edca9a4e
Doordashcon <90750465+Doordashcon@users.noreply.github.com>
2021-12-11 08:10:32 +0100

    Extract Runtime Constants into Separate Crates (#4456)
    * kusama-runtime-constants created
    
    * polkadot_runtime_constants added
    
    * runtime constants extracted
    
    * update node
    
    * cargo +nightly fmt
    
    * Delete constants.rs
    
    * update Cargo.toml
    
    * 2021
    
    * runtime constants update
    
    * utils
    
    * utils
    
    * node test service
    
    * Update runtime/rococo/constants/Cargo.toml
    
    Co-authored-by: Bastian Köcher <bkchr@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Bastian Köcher <bkchr@users.noreply.github.com>
    
    * runtime-native
    
    * bridge messages
    
    * re-export DOLLARS
    
    * Update runtime/westend/Cargo.toml
    
    Co-authored-by: Bastian Köcher <bkchr@users.noreply.github.com>
    
    * Update runtime/test-runtime/Cargo.toml
    
    Co-authored-by: Bastian Köcher <bkchr@users.noreply.github.com>
    
    * std feature
    
    Co-authored-by: Bastian Köcher <bkchr@users.noreply.github.com>

--- relay/runtimes/polkadot/constants/src/lib.rs
+++ relay/runtimes/polkadot/constants/src/lib.rs
@@ -14,17 +14,21 @@
 // You should have received a copy of the GNU General Public License
 // along with Polkadot.  If not, see <http://www.gnu.org/licenses/>.
 
+#![cfg_attr(not(feature = "std"), no_std)]
+
+pub use self::currency::DOLLARS;
+
 /// Money matters.
 pub mod currency {
 	use primitives::v0::Balance;
 
-	pub const UNITS: Balance = 1_000_000_000_000;
-	pub const CENTS: Balance = UNITS / 30_000;
-	pub const GRAND: Balance = CENTS * 100_000;
-	pub const MILLICENTS: Balance = CENTS / 1_000;
+	pub const UNITS: Balance = 10_000_000_000;
+	pub const DOLLARS: Balance = UNITS; // 10_000_000_000
+	pub const CENTS: Balance = DOLLARS / 100; // 100_000_000
+	pub const MILLICENTS: Balance = CENTS / 1_000; // 100_000
 
 	pub const fn deposit(items: u32, bytes: u32) -> Balance {
-		items as Balance * 2_000 * CENTS + (bytes as Balance) * 100 * MILLICENTS
+		items as Balance * 20 * DOLLARS + (bytes as Balance) * 100 * MILLICENTS
 	}
 }
 
@@ -33,7 +37,7 @@ pub mod time {
 	use primitives::v0::{BlockNumber, Moment};
 	pub const MILLISECS_PER_BLOCK: Moment = 6000;
 	pub const SLOT_DURATION: Moment = MILLISECS_PER_BLOCK;
-	pub const EPOCH_DURATION_IN_SLOTS: BlockNumber = 1 * HOURS;
+	pub const EPOCH_DURATION_IN_SLOTS: BlockNumber = 4 * HOURS;
 
 	// These time units are defined in number of blocks.
 	pub const MINUTES: BlockNumber = 60_000 / (MILLISECS_PER_BLOCK as BlockNumber);
@@ -72,7 +76,7 @@ pub mod fee {
 	impl WeightToFeePolynomial for WeightToFee {
 		type Balance = Balance;
 		fn polynomial() -> WeightToFeeCoefficients<Self::Balance> {
-			// in Kusama, extrinsic base weight (smallest non-zero weight) is mapped to 1/10 CENT:
+			// in Polkadot, extrinsic base weight (smallest non-zero weight) is mapped to 1/10 CENT:
 			let p = super::currency::CENTS;
 			let q = 10 * Balance::from(ExtrinsicBaseWeight::get());
 			smallvec![WeightToFeeCoefficient {
@@ -88,7 +92,7 @@ pub mod fee {
 #[cfg(test)]
 mod tests {
 	use super::{
-		currency::{CENTS, MILLICENTS},
+		currency::{CENTS, DOLLARS, MILLICENTS},
 		fee::WeightToFee,
 	};
 	use frame_support::weights::WeightToFeePolynomial;
@@ -97,10 +101,10 @@ mod tests {
 	#[test]
 	// This function tests that the fee for `MAXIMUM_BLOCK_WEIGHT` of weight is correct
 	fn full_block_fee_is_correct() {
-		// A full block should cost 1,600 CENTS
+		// A full block should cost 16 DOLLARS
 		println!("Base: {}", ExtrinsicBaseWeight::get());
 		let x = WeightToFee::calc(&MAXIMUM_BLOCK_WEIGHT);
-		let y = 16 * 100 * CENTS;
+		let y = 16 * DOLLARS;
 		assert!(x.max(y) - x.min(y) < MILLICENTS);
 	}
 
