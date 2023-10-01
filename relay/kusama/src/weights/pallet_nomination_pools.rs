a9a04c8e4803c2fc2aadc3d13010e7583dd27eb0
Ankan <ankan.anurag@gmail.com>
2023-10-01 02:52:36 +0200

    todo: bench for the new call

--- relay/kusama/src/weights/pallet_nomination_pools.rs
+++ relay/kusama/src/weights/pallet_nomination_pools.rs
@@ -597,4 +597,8 @@ impl<T: frame_system::Config> pallet_nomination_pools::WeightInfo for WeightInfo
 			.saturating_add(T::DbWeight::get().reads(4))
 			.saturating_add(T::DbWeight::get().writes(2))
 	}
+
+	fn adjust_pool_deposit() -> Weight {
+		todo!("run benchmarks")
+	}
 }
