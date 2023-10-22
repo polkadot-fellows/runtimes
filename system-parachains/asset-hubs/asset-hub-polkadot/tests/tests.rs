732d788ed5af64f581abc108f8e14cb375736576
Liam Aharon <liam.aharon@hotmail.com>
2023-10-23 09:58:21 +1100

    Update system-parachains/asset-hubs/asset-hub-polkadot/tests/tests.rs
    Co-authored-by: joe petrowski <25483142+joepetrowski@users.noreply.github.com>

--- system-parachains/asset-hubs/asset-hub-polkadot/tests/tests.rs
+++ system-parachains/asset-hubs/asset-hub-polkadot/tests/tests.rs
@@ -15,7 +15,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//! Tests for the Polkadot Assets Hub (previously known as Statemint) chain.
+//! Tests for the Polkadot Asset Hub (previously known as Statemint) chain.
 
 use asset_hub_polkadot_runtime::xcm_config::{
 	AssetFeeAsExistentialDepositMultiplierFeeCharger, CheckingAccount, DotLocation,
