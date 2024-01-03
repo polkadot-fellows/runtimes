332998dbd401f6e7beeb0c76385e9575d24680f7
Dónal Murray <donal.murray@parity.io>
2024-01-03 13:49:58 +0000

    Remove constants mod

--- system-parachains/constants/src/lib.rs
+++ system-parachains/constants/src/lib.rs
@@ -19,46 +19,39 @@
 pub mod kusama;
 pub mod polkadot;
 
-pub use constants::*;
-
-/// re-export types from upstream
+use frame_support::{
+	weights::{constants::WEIGHT_REF_TIME_PER_SECOND, Weight},
+	PalletId,
+};
 pub use parachains_common::BlockNumber;
+use sp_runtime::Perbill;
 
-/// Common constants of parachains.
-mod constants {
-	use super::BlockNumber;
-	use frame_support::{
-		weights::{constants::WEIGHT_REF_TIME_PER_SECOND, Weight},
-		PalletId,
-	};
-	use sp_runtime::Perbill;
-	/// This determines the average expected block time that we are targeting. Blocks will be
-	/// produced at a minimum duration defined by `SLOT_DURATION`. `SLOT_DURATION` is picked up by
-	/// `pallet_timestamp` which is in turn picked up by `pallet_aura` to implement `fn
-	/// slot_duration()`.
-	///
-	/// Change this to adjust the block time.
-	pub const MILLISECS_PER_BLOCK: u64 = 12000;
-	pub const SLOT_DURATION: u64 = MILLISECS_PER_BLOCK;
+/// This determines the average expected block time that we are targeting. Blocks will be
+/// produced at a minimum duration defined by `SLOT_DURATION`. `SLOT_DURATION` is picked up by
+/// `pallet_timestamp` which is in turn picked up by `pallet_aura` to implement `fn
+/// slot_duration()`.
+///
+/// Change this to adjust the block time.
+pub const MILLISECS_PER_BLOCK: u64 = 12000;
+pub const SLOT_DURATION: u64 = MILLISECS_PER_BLOCK;
 
-	// Time is measured by number of blocks.
-	pub const MINUTES: BlockNumber = 60_000 / (MILLISECS_PER_BLOCK as BlockNumber);
-	pub const HOURS: BlockNumber = MINUTES * 60;
-	pub const DAYS: BlockNumber = HOURS * 24;
+// Time is measured by number of blocks.
+pub const MINUTES: BlockNumber = 60_000 / (MILLISECS_PER_BLOCK as BlockNumber);
+pub const HOURS: BlockNumber = MINUTES * 60;
+pub const DAYS: BlockNumber = HOURS * 24;
 
-	/// We assume that ~5% of the block weight is consumed by `on_initialize` handlers. This is
-	/// used to limit the maximal weight of a single extrinsic.
-	pub const AVERAGE_ON_INITIALIZE_RATIO: Perbill = Perbill::from_percent(5);
-	/// We allow `Normal` extrinsics to fill up the block up to 75%, the rest can be used by
-	/// Operational  extrinsics.
-	pub const NORMAL_DISPATCH_RATIO: Perbill = Perbill::from_percent(75);
+/// We assume that ~5% of the block weight is consumed by `on_initialize` handlers. This is
+/// used to limit the maximal weight of a single extrinsic.
+pub const AVERAGE_ON_INITIALIZE_RATIO: Perbill = Perbill::from_percent(5);
+/// We allow `Normal` extrinsics to fill up the block up to 75%, the rest can be used by
+/// Operational  extrinsics.
+pub const NORMAL_DISPATCH_RATIO: Perbill = Perbill::from_percent(75);
 
-	/// We allow for 0.5 seconds of compute with a 6 second average block time.
-	pub const MAXIMUM_BLOCK_WEIGHT: Weight = Weight::from_parts(
-		WEIGHT_REF_TIME_PER_SECOND.saturating_div(2),
-		polkadot_primitives::MAX_POV_SIZE as u64,
-	);
+/// We allow for 0.5 seconds of compute with a 6 second average block time.
+pub const MAXIMUM_BLOCK_WEIGHT: Weight = Weight::from_parts(
+	WEIGHT_REF_TIME_PER_SECOND.saturating_div(2),
+	polkadot_primitives::MAX_POV_SIZE as u64,
+);
 
-	/// Treasury pallet id of the local chain, used to convert into AccountId
-	pub const TREASURY_PALLET_ID: PalletId = PalletId(*b"py/trsry");
-}
+/// Treasury pallet id of the local chain, used to convert into AccountId
+pub const TREASURY_PALLET_ID: PalletId = PalletId(*b"py/trsry");
