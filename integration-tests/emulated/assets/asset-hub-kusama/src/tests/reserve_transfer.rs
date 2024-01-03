09136edce2300a4320627da5be6a40bdef4844fc
Adrian Catangiu <adrian@parity.io>
2024-01-03 14:48:35 +0200

    remove tests for deprecated reserve_transfer_assets()

--- integration-tests/emulated/assets/asset-hub-kusama/src/tests/reserve_transfer.rs
+++ integration-tests/emulated/assets/asset-hub-kusama/src/tests/reserve_transfer.rs
@@ -105,16 +105,6 @@ fn relay_limited_reserve_transfer_assets(t: RelayToSystemParaTest) -> DispatchRe
 	)
 }
 
-fn relay_reserve_transfer_assets(t: RelayToSystemParaTest) -> DispatchResult {
-	<Kusama as KusamaPallet>::XcmPallet::reserve_transfer_assets(
-		t.signed_origin,
-		bx!(t.args.dest.into()),
-		bx!(t.args.beneficiary.into()),
-		bx!(t.args.assets.into()),
-		t.args.fee_asset_item,
-	)
-}
-
 fn system_para_limited_reserve_transfer_assets(t: SystemParaToRelayTest) -> DispatchResult {
 	<AssetHubKusama as AssetHubKusamaPallet>::PolkadotXcm::limited_reserve_transfer_assets(
 		t.signed_origin,
@@ -126,16 +116,6 @@ fn system_para_limited_reserve_transfer_assets(t: SystemParaToRelayTest) -> Disp
 	)
 }
 
-fn system_para_reserve_transfer_assets(t: SystemParaToRelayTest) -> DispatchResult {
-	<AssetHubKusama as AssetHubKusamaPallet>::PolkadotXcm::reserve_transfer_assets(
-		t.signed_origin,
-		bx!(t.args.dest.into()),
-		bx!(t.args.beneficiary.into()),
-		bx!(t.args.assets.into()),
-		t.args.fee_asset_item,
-	)
-}
-
 fn system_para_to_para_limited_reserve_transfer_assets(t: SystemParaToParaTest) -> DispatchResult {
 	<AssetHubKusama as AssetHubKusamaPallet>::PolkadotXcm::limited_reserve_transfer_assets(
 		t.signed_origin,
@@ -144,16 +124,6 @@ fn system_para_to_para_limited_reserve_transfer_assets(t: SystemParaToParaTest) 
 		bx!(t.args.assets.into()),
 		t.args.fee_asset_item,
 		t.args.weight_limit,
-	)
-}
-
-fn system_para_to_para_reserve_transfer_assets(t: SystemParaToParaTest) -> DispatchResult {
-	<AssetHubKusama as AssetHubKusamaPallet>::PolkadotXcm::reserve_transfer_assets(
-		t.signed_origin,
-		bx!(t.args.dest.into()),
-		bx!(t.args.beneficiary.into()),
-		bx!(t.args.assets.into()),
-		t.args.fee_asset_item,
 	)
 }
 
@@ -217,65 +187,6 @@ fn limited_reserve_transfer_native_asset_from_system_para_to_relay_fails() {
 	assert_eq!(receiver_balance_before, receiver_balance_after);
 }
 
-/// Reserve Transfers of native asset from Relay Chain to the System Parachain shouldn't work
-#[test]
-fn reserve_transfer_native_asset_from_relay_to_system_para_fails() {
-	// Init values for Relay Chain
-	let amount_to_send: Balance = KUSAMA_ED * 1000;
-	let test_args = TestContext {
-		sender: KusamaSender::get(),
-		receiver: AssetHubKusamaReceiver::get(),
-		args: relay_test_args(amount_to_send),
-	};
-
-	let mut test = RelayToSystemParaTest::new(test_args);
-
-	let sender_balance_before = test.sender.balance;
-	let receiver_balance_before = test.receiver.balance;
-
-	test.set_assertion::<Kusama>(relay_origin_assertions);
-	test.set_assertion::<AssetHubKusama>(system_para_dest_assertions_incomplete);
-	test.set_dispatchable::<Kusama>(relay_reserve_transfer_assets);
-	test.assert();
-
-	let sender_balance_after = test.sender.balance;
-	let receiver_balance_after = test.receiver.balance;
-
-	assert!(sender_balance_before - amount_to_send >= sender_balance_after);
-	assert_eq!(receiver_balance_before, receiver_balance_after);
-}
-
-/// Reserve Transfers of native asset from System Parachain to Relay Chain shouldn't work
-#[test]
-fn reserve_transfer_native_asset_from_system_para_to_relay_fails() {
-	// Init values for System Parachain
-	let destination = AssetHubKusama::parent_location();
-	let beneficiary_id = KusamaReceiver::get();
-	let amount_to_send: Balance = ASSET_HUB_KUSAMA_ED * 1000;
-	let assets = (Parent, amount_to_send).into();
-
-	let test_args = TestContext {
-		sender: AssetHubKusamaSender::get(),
-		receiver: KusamaReceiver::get(),
-		args: system_para_test_args(destination, beneficiary_id, amount_to_send, assets, None),
-	};
-
-	let mut test = SystemParaToRelayTest::new(test_args);
-
-	let sender_balance_before = test.sender.balance;
-	let receiver_balance_before = test.receiver.balance;
-
-	test.set_assertion::<AssetHubKusama>(system_para_to_relay_assertions);
-	test.set_dispatchable::<AssetHubKusama>(system_para_reserve_transfer_assets);
-	test.assert();
-
-	let sender_balance_after = test.sender.balance;
-	let receiver_balance_after = test.receiver.balance;
-
-	assert_eq!(sender_balance_before, sender_balance_after);
-	assert_eq!(receiver_balance_before, receiver_balance_after);
-}
-
 /// Limited Reserve Transfers of native asset from System Parachain to Parachain should work
 #[test]
 fn limited_reserve_transfer_native_asset_from_system_para_to_para() {
@@ -299,38 +210,6 @@ fn limited_reserve_transfer_native_asset_from_system_para_to_para() {
 	// TODO: Add assertion for Penpal runtime. Right now message is failing with
 	// `UntrustedReserveLocation`
 	test.set_dispatchable::<AssetHubKusama>(system_para_to_para_limited_reserve_transfer_assets);
-	test.assert();
-
-	let sender_balance_after = test.sender.balance;
-
-	assert!(sender_balance_before - amount_to_send >= sender_balance_after);
-	// TODO: Check receiver balance when Penpal runtime is improved to propery handle reserve
-	// transfers
-}
-
-/// Reserve Transfers of native asset from System Parachain to Parachain should work
-#[test]
-fn reserve_transfer_native_asset_from_system_para_to_para() {
-	// Init values for System Parachain
-	let destination = AssetHubKusama::sibling_location_of(PenpalKusamaA::para_id());
-	let beneficiary_id = PenpalKusamaAReceiver::get();
-	let amount_to_send: Balance = ASSET_HUB_KUSAMA_ED * 1000;
-	let assets = (Parent, amount_to_send).into();
-
-	let test_args = TestContext {
-		sender: AssetHubKusamaSender::get(),
-		receiver: PenpalKusamaAReceiver::get(),
-		args: system_para_test_args(destination, beneficiary_id, amount_to_send, assets, None),
-	};
-
-	let mut test = SystemParaToParaTest::new(test_args);
-
-	let sender_balance_before = test.sender.balance;
-
-	test.set_assertion::<AssetHubKusama>(system_para_to_para_assertions);
-	// TODO: Add assertion for Penpal runtime. Right now message is failing with
-	// `UntrustedReserveLocation`
-	test.set_dispatchable::<AssetHubKusama>(system_para_to_para_reserve_transfer_assets);
 	test.assert();
 
 	let sender_balance_after = test.sender.balance;
@@ -372,40 +251,5 @@ fn limited_reserve_transfer_asset_from_system_para_to_para() {
 	// TODO: Add assertions when Penpal is able to manage assets
 	system_para_test
 		.set_dispatchable::<AssetHubKusama>(system_para_to_para_limited_reserve_transfer_assets);
-	system_para_test.assert();
-}
-
-/// Reserve Transfers of a local asset from System Parachain to Parachain should work
-#[test]
-fn reserve_transfer_asset_from_system_para_to_para() {
-	// Force create asset from Relay Chain and mint assets for System Parachain's sender account
-	AssetHubKusama::force_create_and_mint_asset(
-		ASSET_ID,
-		ASSET_MIN_BALANCE,
-		true,
-		AssetHubKusamaSender::get(),
-		ASSET_MIN_BALANCE * 1000000,
-	);
-
-	// Init values for System Parachain
-	let destination = AssetHubKusama::sibling_location_of(PenpalKusamaA::para_id());
-	let beneficiary_id = PenpalKusamaAReceiver::get();
-	let amount_to_send = ASSET_MIN_BALANCE * 1000;
-	let assets =
-		(X2(PalletInstance(ASSETS_PALLET_ID), GeneralIndex(ASSET_ID.into())), amount_to_send)
-			.into();
-
-	let system_para_test_args = TestContext {
-		sender: AssetHubKusamaSender::get(),
-		receiver: PenpalKusamaAReceiver::get(),
-		args: system_para_test_args(destination, beneficiary_id, amount_to_send, assets, None),
-	};
-
-	let mut system_para_test = SystemParaToParaTest::new(system_para_test_args);
-
-	system_para_test.set_assertion::<AssetHubKusama>(system_para_to_para_assets_assertions);
-	// TODO: Add assertions when Penpal is able to manage assets
-	system_para_test
-		.set_dispatchable::<AssetHubKusama>(system_para_to_para_reserve_transfer_assets);
 	system_para_test.assert();
 }
--- integration-tests/emulated/assets/asset-hub-polkadot/src/tests/reserve_transfer.rs
+++ integration-tests/emulated/assets/asset-hub-polkadot/src/tests/reserve_transfer.rs
@@ -105,16 +105,6 @@ fn relay_limited_reserve_transfer_assets(t: RelayToSystemParaTest) -> DispatchRe
 	)
 }
 
-fn relay_reserve_transfer_assets(t: RelayToSystemParaTest) -> DispatchResult {
-	<Polkadot as PolkadotPallet>::XcmPallet::reserve_transfer_assets(
-		t.signed_origin,
-		bx!(t.args.dest.into()),
-		bx!(t.args.beneficiary.into()),
-		bx!(t.args.assets.into()),
-		t.args.fee_asset_item,
-	)
-}
-
 fn system_para_limited_reserve_transfer_assets(t: SystemParaToRelayTest) -> DispatchResult {
 	<AssetHubPolkadot as AssetHubPolkadotPallet>::PolkadotXcm::limited_reserve_transfer_assets(
 		t.signed_origin,
@@ -126,16 +116,6 @@ fn system_para_limited_reserve_transfer_assets(t: SystemParaToRelayTest) -> Disp
 	)
 }
 
-fn system_para_reserve_transfer_assets(t: SystemParaToRelayTest) -> DispatchResult {
-	<AssetHubPolkadot as AssetHubPolkadotPallet>::PolkadotXcm::reserve_transfer_assets(
-		t.signed_origin,
-		bx!(t.args.dest.into()),
-		bx!(t.args.beneficiary.into()),
-		bx!(t.args.assets.into()),
-		t.args.fee_asset_item,
-	)
-}
-
 fn system_para_to_para_limited_reserve_transfer_assets(t: SystemParaToParaTest) -> DispatchResult {
 	<AssetHubPolkadot as AssetHubPolkadotPallet>::PolkadotXcm::limited_reserve_transfer_assets(
 		t.signed_origin,
@@ -144,16 +124,6 @@ fn system_para_to_para_limited_reserve_transfer_assets(t: SystemParaToParaTest) 
 		bx!(t.args.assets.into()),
 		t.args.fee_asset_item,
 		t.args.weight_limit,
-	)
-}
-
-fn system_para_to_para_reserve_transfer_assets(t: SystemParaToParaTest) -> DispatchResult {
-	<AssetHubPolkadot as AssetHubPolkadotPallet>::PolkadotXcm::reserve_transfer_assets(
-		t.signed_origin,
-		bx!(t.args.dest.into()),
-		bx!(t.args.beneficiary.into()),
-		bx!(t.args.assets.into()),
-		t.args.fee_asset_item,
 	)
 }
 
@@ -217,65 +187,6 @@ fn limited_reserve_transfer_native_asset_from_system_para_to_relay_fails() {
 	assert_eq!(receiver_balance_before, receiver_balance_after);
 }
 
-/// Reserve Transfers of native asset from Relay Chain to the System Parachain shouldn't work
-#[test]
-fn reserve_transfer_native_asset_from_relay_to_system_para_fails() {
-	// Init values for Relay Chain
-	let amount_to_send: Balance = POLKADOT_ED * 1000;
-	let test_args = TestContext {
-		sender: PolkadotSender::get(),
-		receiver: AssetHubPolkadotReceiver::get(),
-		args: relay_test_args(amount_to_send),
-	};
-
-	let mut test = RelayToSystemParaTest::new(test_args);
-
-	let sender_balance_before = test.sender.balance;
-	let receiver_balance_before = test.receiver.balance;
-
-	test.set_assertion::<Polkadot>(relay_origin_assertions);
-	test.set_assertion::<AssetHubPolkadot>(system_para_dest_assertions_incomplete);
-	test.set_dispatchable::<Polkadot>(relay_reserve_transfer_assets);
-	test.assert();
-
-	let sender_balance_after = test.sender.balance;
-	let receiver_balance_after = test.receiver.balance;
-
-	assert!(sender_balance_before - amount_to_send >= sender_balance_after);
-	assert_eq!(receiver_balance_before, receiver_balance_after);
-}
-
-/// Reserve Transfers of native asset from System Parachain to Relay Chain shouldn't work
-#[test]
-fn reserve_transfer_native_asset_from_system_para_to_relay_fails() {
-	// Init values for System Parachain
-	let destination = AssetHubPolkadot::parent_location();
-	let beneficiary_id = PolkadotReceiver::get();
-	let amount_to_send: Balance = ASSET_HUB_POLKADOT_ED * 1000;
-	let assets = (Parent, amount_to_send).into();
-
-	let test_args = TestContext {
-		sender: AssetHubPolkadotSender::get(),
-		receiver: PolkadotReceiver::get(),
-		args: system_para_test_args(destination, beneficiary_id, amount_to_send, assets, None),
-	};
-
-	let mut test = SystemParaToRelayTest::new(test_args);
-
-	let sender_balance_before = test.sender.balance;
-	let receiver_balance_before = test.receiver.balance;
-
-	test.set_assertion::<AssetHubPolkadot>(system_para_to_relay_assertions);
-	test.set_dispatchable::<AssetHubPolkadot>(system_para_reserve_transfer_assets);
-	test.assert();
-
-	let sender_balance_after = test.sender.balance;
-	let receiver_balance_after = test.receiver.balance;
-
-	assert_eq!(sender_balance_before, sender_balance_after);
-	assert_eq!(receiver_balance_before, receiver_balance_after);
-}
-
 /// Limited Reserve Transfers of native asset from System Parachain to Parachain should work
 #[test]
 fn limited_reserve_transfer_native_asset_from_system_para_to_para() {
@@ -299,38 +210,6 @@ fn limited_reserve_transfer_native_asset_from_system_para_to_para() {
 	// TODO: Add assertion for Penpal runtime. Right now message is failing with
 	// `UntrustedReserveLocation`
 	test.set_dispatchable::<AssetHubPolkadot>(system_para_to_para_limited_reserve_transfer_assets);
-	test.assert();
-
-	let sender_balance_after = test.sender.balance;
-
-	assert!(sender_balance_before - amount_to_send >= sender_balance_after);
-	// TODO: Check receiver balance when Penpal runtime is improved to propery handle reserve
-	// transfers
-}
-
-/// Reserve Transfers of native asset from System Parachain to Parachain should work
-#[test]
-fn reserve_transfer_native_asset_from_system_para_to_para() {
-	// Init values for System Parachain
-	let destination = AssetHubPolkadot::sibling_location_of(PenpalPolkadotA::para_id());
-	let beneficiary_id = PenpalPolkadotAReceiver::get();
-	let amount_to_send: Balance = ASSET_HUB_POLKADOT_ED * 1000;
-	let assets = (Parent, amount_to_send).into();
-
-	let test_args = TestContext {
-		sender: AssetHubPolkadotSender::get(),
-		receiver: PenpalPolkadotAReceiver::get(),
-		args: system_para_test_args(destination, beneficiary_id, amount_to_send, assets, None),
-	};
-
-	let mut test = SystemParaToParaTest::new(test_args);
-
-	let sender_balance_before = test.sender.balance;
-
-	test.set_assertion::<AssetHubPolkadot>(system_para_to_para_assertions);
-	// TODO: Add assertion for Penpal runtime. Right now message is failing with
-	// `UntrustedReserveLocation`
-	test.set_dispatchable::<AssetHubPolkadot>(system_para_to_para_reserve_transfer_assets);
 	test.assert();
 
 	let sender_balance_after = test.sender.balance;
@@ -372,40 +251,5 @@ fn limited_reserve_transfer_asset_from_system_para_to_para() {
 	// TODO: Add assertions when Penpal is able to manage assets
 	system_para_test
 		.set_dispatchable::<AssetHubPolkadot>(system_para_to_para_limited_reserve_transfer_assets);
-	system_para_test.assert();
-}
-
-/// Reserve Transfers of a local asset from System Parachain to Parachain should work
-#[test]
-fn reserve_transfer_asset_from_system_para_to_para() {
-	// Force create asset from Relay Chain and mint assets for System Parachain's sender account
-	AssetHubPolkadot::force_create_and_mint_asset(
-		ASSET_ID,
-		ASSET_MIN_BALANCE,
-		true,
-		AssetHubPolkadotSender::get(),
-		ASSET_MIN_BALANCE * 1000000,
-	);
-
-	// Init values for System Parachain
-	let destination = AssetHubPolkadot::sibling_location_of(PenpalPolkadotA::para_id());
-	let beneficiary_id = PenpalPolkadotAReceiver::get();
-	let amount_to_send = ASSET_MIN_BALANCE * 1000;
-	let assets =
-		(X2(PalletInstance(ASSETS_PALLET_ID), GeneralIndex(ASSET_ID.into())), amount_to_send)
-			.into();
-
-	let system_para_test_args = TestContext {
-		sender: AssetHubPolkadotSender::get(),
-		receiver: PenpalPolkadotAReceiver::get(),
-		args: system_para_test_args(destination, beneficiary_id, amount_to_send, assets, None),
-	};
-
-	let mut system_para_test = SystemParaToParaTest::new(system_para_test_args);
-
-	system_para_test.set_assertion::<AssetHubPolkadot>(system_para_to_para_assets_assertions);
-	// TODO: Add assertions when Penpal is able to manage assets
-	system_para_test
-		.set_dispatchable::<AssetHubPolkadot>(system_para_to_para_reserve_transfer_assets);
 	system_para_test.assert();
 }
