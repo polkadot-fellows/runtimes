ba94fff6a299358fb83f1b3a0000b4fb8f4f8b02
Branislav Kontur <bkontur@gmail.com>
2024-01-08 15:08:33 +0100

    fmt

--- system-parachains/asset-hubs/asset-hub-kusama/src/lib.rs
+++ system-parachains/asset-hubs/asset-hub-kusama/src/lib.rs
@@ -1366,7 +1366,8 @@ impl_runtime_apis! {
 				}
 
 				fn universal_alias() -> Result<(MultiLocation, Junction), BenchmarkError> {
-xcm_config::bridging::BridgingBenchmarksHelper::prepare_universal_alias().ok_or(BenchmarkError::Skip)
+					xcm_config::bridging::BridgingBenchmarksHelper::prepare_universal_alias()
+					.ok_or(BenchmarkError::Skip)
 				}
 
 				fn transact_origin_and_runtime_call() -> Result<(MultiLocation, RuntimeCall), BenchmarkError> {
