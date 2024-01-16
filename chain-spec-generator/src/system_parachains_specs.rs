914c7843787ce3c95ff23b3bcfd090b90d5878a9
Liam Aharon <liam.aharon@hotmail.com>
2024-01-16 20:15:07 +0400

    copyright

--- chain-spec-generator/src/system_parachains_specs.rs
+++ chain-spec-generator/src/system_parachains_specs.rs
@@ -1,3 +1,20 @@
+// Copyright (C) Parity Technologies and the various Polkadot contributors, see Contributions.md
+// for a list of specific contributors.
+// This file is part of Polkadot.
+
+// Polkadot is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Polkadot is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Polkadot.  If not, see <http://www.gnu.org/licenses/>.
+
 use crate::common::{get_account_id_from_seed, get_from_seed, testnet_accounts};
 use cumulus_primitives_core::ParaId;
 use parachains_common::{AccountId, AssetHubPolkadotAuraId, AuraId, Balance};
