4c7d743a7f770e30b3aef88c9ed3e0b9da760e6b
cheme <emericchevalier.pro@gmail.com>
2024-02-14 15:48:50 +0100

    new error

--- relay/kusama/src/lib.rs
+++ relay/kusama/src/lib.rs
@@ -2664,7 +2664,7 @@ mod clean_state_migration {
 
 	impl OnRuntimeUpgrade for CleanMigrate {
 		#[cfg(feature = "try-runtime")]
-		fn pre_upgrade() -> Result<Vec<u8>, &'static str> {
+		fn pre_upgrade() -> Result<Vec<u8>, sp_runtime::DispatchErroR> {
 			Ok(Default::default())
 		}
 
@@ -2676,7 +2676,7 @@ mod clean_state_migration {
 		}
 
 		#[cfg(feature = "try-runtime")]
-		fn post_upgrade(_state: Vec<u8>) -> Result<(), &'static str> {
+		fn post_upgrade(_state: Vec<u8>) -> Result<(), sp_runtime::DispatchError> {
 			frame_support::ensure!(
 				!AutoLimits::exists() && !SignedMigrationMaxLimits::exists(),
 				"State migration clean.",
