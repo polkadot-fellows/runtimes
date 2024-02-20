2ab60459a1448f0a22850a876516d111f6ed2234
Branislav Kontur <bkontur@gmail.com>
2024-02-20 17:25:00 +0100

    [pallet_xcm] Added `pallet_xcm::migration::MigrateToLatestXcmVersion<Runtime>`

--- system-parachains/bridge-hubs/bridge-hub-kusama/src/lib.rs
+++ system-parachains/bridge-hubs/bridge-hub-kusama/src/lib.rs
@@ -126,6 +126,8 @@ pub type UncheckedExtrinsic =
 pub type Migrations = (
 	// unreleased
 	cumulus_pallet_xcmp_queue::migration::v4::MigrationToV4<Runtime>,
+	// permanent
+	pallet_xcm::migration::MigrateToLatestXcmVersion<Runtime>,
 );
 
 /// Executive: handles dispatch to the various modules.
