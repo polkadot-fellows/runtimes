81735e021b742d610c0356a83470dd32a335bb3e
claravanstaden <Cats 4 life!>
2024-02-22 08:56:05 +0200

    kusama bridge hub snowbridge tests

--- system-parachains/bridge-hubs/bridge-hub-kusama/tests/snowbridge.rs
+++ system-parachains/bridge-hubs/bridge-hub-kusama/tests/snowbridge.rs
@@ -25,15 +25,32 @@ use bridge_hub_kusama_runtime::{
 };
 use codec::{Decode, Encode};
 use cumulus_primitives_core::XcmError::{FailedToTransactAsset, NotHoldingFees};
-use frame_support::parameter_types;
+use frame_system::pallet_prelude::BlockNumberFor;
+use snowbridge_core::{ChannelId, ParaId};
+use frame_support::{assert_err, assert_ok, parameter_types, traits::{OnFinalize, OnInitialize},};
 use parachains_common::{AccountId, AuraId, Balance};
 use snowbridge_pallet_ethereum_client::WeightInfo;
 use sp_core::H160;
 use sp_keyring::AccountKeyring::Alice;
 use sp_runtime::{
+    traits::Header,
     generic::{Era, SignedPayload},
-    AccountId32,
+    AccountId32, Saturating
 };
+use bridge_hub_test_utils::ValidatorIdOf;
+use parachains_runtimes_test_utils::{
+    AccountIdOf, CollatorSessionKeys, ExtBuilder, XcmReceivedFrom,
+};
+pub use parachains_runtimes_test_utils::test_cases::change_storage_constant_by_governance_works;
+use xcm::{
+    latest::prelude::*,
+    v3::Error::self,
+};
+use xcm_executor::XcmExecutor;
+use snowbridge_runtime_test_common::initial_fund;
+
+type RuntimeHelper<Runtime, AllPalletsWithoutSystem = ()> =
+parachains_runtimes_test_utils::RuntimeHelper<Runtime, AllPalletsWithoutSystem>;
 
 parameter_types! {
 		pub const DefaultBridgeHubEthereumBaseFee: Balance = 2_750_872_500_000;
@@ -49,7 +66,7 @@ fn collator_session_keys() -> bridge_hub_test_utils::CollatorSessionKeys<Runtime
 
 #[test]
 pub fn transfer_token_to_ethereum_works() {
-    snowbridge_runtime_test_common::send_transfer_token_message_success::<Runtime, XcmConfig>(
+    send_transfer_token_message_success::<Runtime, XcmConfig>(
         collator_session_keys(),
         1013,
         1000,
@@ -78,7 +95,7 @@ pub fn unpaid_transfer_token_to_ethereum_fails_with_barrier() {
 
 #[test]
 pub fn transfer_token_to_ethereum_fee_not_enough() {
-    snowbridge_runtime_test_common::send_transfer_token_message_failure::<Runtime, XcmConfig>(
+    send_transfer_token_message_failure::<Runtime, XcmConfig>(
         collator_session_keys(),
         1013,
         1000,
@@ -93,7 +110,7 @@ pub fn transfer_token_to_ethereum_fee_not_enough() {
 
 #[test]
 pub fn transfer_token_to_ethereum_insufficient_fund() {
-    snowbridge_runtime_test_common::send_transfer_token_message_failure::<Runtime, XcmConfig>(
+    send_transfer_token_message_failure::<Runtime, XcmConfig>(
         collator_session_keys(),
         1013,
         1000,
@@ -103,6 +120,55 @@ pub fn transfer_token_to_ethereum_insufficient_fund() {
         DefaultBridgeHubEthereumBaseFee::get(),
         FailedToTransactAsset("Funds are unavailable"),
     )
+}
+
+#[allow(clippy::too_many_arguments)]
+pub fn send_transfer_token_message_failure<Runtime, XcmConfig>(
+    collator_session_key: CollatorSessionKeys<Runtime>,
+    runtime_para_id: u32,
+    assethub_parachain_id: u32,
+    initial_amount: u128,
+    weth_contract_address: H160,
+    destination_address: H160,
+    fee_amount: u128,
+    expected_error: Error,
+) where
+    Runtime: frame_system::Config
+    + pallet_balances::Config
+    + pallet_session::Config
+    + pallet_xcm::Config
+    + parachain_info::Config
+    + pallet_collator_selection::Config
+    + cumulus_pallet_parachain_system::Config
+    + snowbridge_pallet_outbound_queue::Config
+    + snowbridge_pallet_system::Config,
+    XcmConfig: xcm_executor::Config,
+    ValidatorIdOf<Runtime>: From<AccountIdOf<Runtime>>,
+{
+    ExtBuilder::<Runtime>::default()
+        .with_collators(collator_session_key.collators())
+        .with_session_keys(collator_session_key.session_keys())
+        .with_para_id(runtime_para_id.into())
+        .with_tracing()
+        .build()
+        .execute_with(|| {
+            <snowbridge_pallet_system::Pallet<Runtime>>::initialize(
+                runtime_para_id.into(),
+                assethub_parachain_id.into(),
+            )
+                .unwrap();
+
+            // fund asset hub sovereign account enough so it can pay fees
+            initial_fund::<Runtime>(assethub_parachain_id, initial_amount);
+
+            let outcome = send_transfer_token_message::<Runtime, XcmConfig>(
+                assethub_parachain_id,
+                weth_contract_address,
+                destination_address,
+                fee_amount,
+            );
+            assert_err!(outcome.ensure_complete(), expected_error);
+        });
 }
 
 #[test]
@@ -170,4 +236,165 @@ fn construct_and_apply_extrinsic(
     let xt = construct_extrinsic(origin, call);
     let r = Executive::apply_extrinsic(xt);
     r.unwrap()
+}
+
+// TODO remove when Ethereum network ID has been extracted as a param
+pub fn send_transfer_token_message<Runtime, XcmConfig>(
+    assethub_parachain_id: u32,
+    weth_contract_address: H160,
+    destination_address: H160,
+    fee_amount: u128,
+) -> Outcome
+    where
+        Runtime: frame_system::Config
+        + pallet_balances::Config
+        + pallet_session::Config
+        + pallet_xcm::Config
+        + parachain_info::Config
+        + pallet_collator_selection::Config
+        + cumulus_pallet_parachain_system::Config
+        + snowbridge_pallet_outbound_queue::Config,
+        XcmConfig: xcm_executor::Config,
+{
+    let assethub_parachain_location = Location::new(1, Parachain(assethub_parachain_id));
+    let asset = Asset {
+        id: AssetId(Location::new(
+            0,
+            [AccountKey20 { network: None, key: weth_contract_address.into() }],
+        )),
+        fun: Fungible(1000000000),
+    };
+    let assets = vec![asset.clone()];
+
+    let inner_xcm = Xcm(vec![
+        WithdrawAsset(Assets::from(assets.clone())),
+        ClearOrigin,
+        BuyExecution { fees: asset, weight_limit: Unlimited },
+        DepositAsset {
+            assets: Wild(All),
+            beneficiary: Location::new(
+                0,
+                [AccountKey20 { network: None, key: destination_address.into() }],
+            ),
+        },
+        SetTopic([0; 32]),
+    ]);
+
+    let fee =
+        Asset { id: AssetId(Location { parents: 1, interior: Here }), fun: Fungible(fee_amount) };
+
+    // prepare transfer token message
+    let xcm = Xcm(vec![
+        WithdrawAsset(Assets::from(vec![fee.clone()])),
+        BuyExecution { fees: fee, weight_limit: Unlimited },
+        ExportMessage {
+            network: Ethereum { chain_id: 1 },
+            destination: Here,
+            xcm: inner_xcm,
+        },
+    ]);
+
+    // execute XCM
+    let mut hash = xcm.using_encoded(sp_io::hashing::blake2_256);
+    XcmExecutor::<XcmConfig>::prepare_and_execute(
+        assethub_parachain_location,
+        xcm,
+        &mut hash,
+        RuntimeHelper::<Runtime>::xcm_max_weight(XcmReceivedFrom::Sibling),
+        Weight::zero(),
+    )
+}
+
+pub fn send_transfer_token_message_success<Runtime, XcmConfig>(
+    collator_session_key: CollatorSessionKeys<Runtime>,
+    runtime_para_id: u32,
+    assethub_parachain_id: u32,
+    weth_contract_address: H160,
+    destination_address: H160,
+    fee_amount: u128,
+    snowbridge_pallet_outbound_queue: Box<
+        dyn Fn(Vec<u8>) -> Option<snowbridge_pallet_outbound_queue::Event<Runtime>>,
+    >,
+) where
+    Runtime: frame_system::Config
+    + pallet_balances::Config
+    + pallet_session::Config
+    + pallet_xcm::Config
+    + parachain_info::Config
+    + pallet_collator_selection::Config
+    + pallet_message_queue::Config
+    + cumulus_pallet_parachain_system::Config
+    + snowbridge_pallet_outbound_queue::Config
+    + snowbridge_pallet_system::Config,
+    XcmConfig: xcm_executor::Config,
+    ValidatorIdOf<Runtime>: From<AccountIdOf<Runtime>>,
+    <Runtime as frame_system::Config>::AccountId: From<sp_runtime::AccountId32> + AsRef<[u8]>,
+{
+    ExtBuilder::<Runtime>::default()
+        .with_collators(collator_session_key.collators())
+        .with_session_keys(collator_session_key.session_keys())
+        .with_para_id(runtime_para_id.into())
+        .with_tracing()
+        .build()
+        .execute_with(|| {
+            <snowbridge_pallet_system::Pallet<Runtime>>::initialize(
+                runtime_para_id.into(),
+                assethub_parachain_id.into(),
+            )
+                .unwrap();
+
+            // fund asset hub sovereign account enough so it can pay fees
+            initial_fund::<Runtime>(assethub_parachain_id, 5_000_000_000_000);
+
+            let outcome = send_transfer_token_message::<Runtime, XcmConfig>(
+                assethub_parachain_id,
+                weth_contract_address,
+                destination_address,
+                fee_amount,
+            );
+
+            assert_ok!(outcome.ensure_complete());
+
+            // check events
+            let mut events = <frame_system::Pallet<Runtime>>::events()
+                .into_iter()
+                .filter_map(|e| snowbridge_pallet_outbound_queue(e.event.encode()));
+            assert!(events.any(|e| matches!(
+				e,
+				snowbridge_pallet_outbound_queue::Event::MessageQueued { .. }
+			)));
+
+            let block_number = <frame_system::Pallet<Runtime>>::block_number();
+            let next_block_number = <frame_system::Pallet<Runtime>>::block_number()
+                .saturating_add(BlockNumberFor::<Runtime>::from(1u32));
+
+            // finish current block
+            <pallet_message_queue::Pallet<Runtime>>::on_finalize(block_number);
+            <snowbridge_pallet_outbound_queue::Pallet<Runtime>>::on_finalize(block_number);
+            <frame_system::Pallet<Runtime>>::on_finalize(block_number);
+
+            // start next block
+            <frame_system::Pallet<Runtime>>::set_block_number(next_block_number);
+            <frame_system::Pallet<Runtime>>::on_initialize(next_block_number);
+            <snowbridge_pallet_outbound_queue::Pallet<Runtime>>::on_initialize(next_block_number);
+            <pallet_message_queue::Pallet<Runtime>>::on_initialize(next_block_number);
+
+            // finish next block
+            <pallet_message_queue::Pallet<Runtime>>::on_finalize(next_block_number);
+            <snowbridge_pallet_outbound_queue::Pallet<Runtime>>::on_finalize(next_block_number);
+            let included_head = <frame_system::Pallet<Runtime>>::finalize();
+
+            let origin: ParaId = assethub_parachain_id.into();
+            let channel_id: ChannelId = origin.into();
+
+            let nonce = snowbridge_pallet_outbound_queue::Nonce::<Runtime>::try_get(channel_id);
+            assert_ok!(nonce);
+            assert_eq!(nonce.unwrap(), 1);
+
+            let digest = included_head.digest();
+
+            //let digest = frame_system::Pallet::<Runtime>::digest();
+            let digest_items = digest.logs();
+            assert!(digest_items.len() == 1 && digest_items[0].as_other().is_some());
+        });
 }
