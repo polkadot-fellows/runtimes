4fbabb79938c668b1a5968bd8aa1b0043db23b82
Dmitry Sinyavin <dmitry.sinyavin@parity.io>
2024-02-29 14:04:29 +0100

    Bump runtime versions to correct ones

--- relay/kusama/src/lib.rs
+++ relay/kusama/src/lib.rs
@@ -156,7 +156,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("kusama"),
 	impl_name: create_runtime_str!("parity-kusama"),
 	authoring_version: 2,
-	spec_version: 1_001_003,
+	spec_version: 1_002_000,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 25,
