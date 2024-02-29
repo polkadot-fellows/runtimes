d77f4af1ba4d7647511ab0fe265c1bfd2548218f
Branislav Kontur <bkontur@gmail.com>
2024-02-29 09:56:52 +0100

    Update system-parachains/asset-hubs/asset-hub-polkadot/src/xcm_config.rs
    Co-authored-by: joe petrowski <25483142+joepetrowski@users.noreply.github.com>

--- system-parachains/asset-hubs/asset-hub-polkadot/src/xcm_config.rs
+++ system-parachains/asset-hubs/asset-hub-polkadot/src/xcm_config.rs
@@ -91,7 +91,7 @@ pub type FungibleTransactor = FungibleAdapter<
 	Balances,
 	// Use this currency when it is a fungible asset matching the given location or name:
 	IsConcrete<DotLocation>,
-	// Convert an XCM Location into a local account id:
+	// Convert an XCM `Location` into a local account ID:
 	LocationToAccountId,
 	// Our chain's account ID type (we can't get away without mentioning it explicitly):
 	AccountId,
