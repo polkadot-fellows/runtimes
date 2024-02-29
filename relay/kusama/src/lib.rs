071894538b9439467d51b5e0cf203c88c75555b8
Alexandru Gheorghe <49718502+alexggh@users.noreply.github.com>
2024-02-29 18:49:03 +0200

    Bump ParachainHost to api version 10 on kusama (#204)
    ... to add approval_voting_params API which will allow us to enable
    approvals coalescing implementation from:
     - https://github.com/paritytech/polkadot-sdk/pull/1191
    
    Note! Bumping the version will not enable the new logic, that will be
    enable at a later date we we decide to call set_approval_voting_params
    with max_approval_coalesce_count greater than 1.
    
    <!-- Remember that you can run `/merge` to enable auto-merge in the PR
    -->
    
    <!-- Remember to modify the changelog. If you don't need to modify it,
    you can check the following box.
    Instead, if you have already modified it, simply delete the following
    line. -->
    
    ---------
    
    Signed-off-by: Alexandru Gheorghe <alexandru.gheorghe@parity.io>

--- relay/kusama/src/lib.rs
+++ relay/kusama/src/lib.rs
@@ -23,12 +23,14 @@
 use pallet_nis::WithMaximumOf;
 use parity_scale_codec::{Decode, Encode, MaxEncodedLen};
 use primitives::{
-	slashing, vstaging::NodeFeatures, AccountId, AccountIndex, Balance, BlockNumber,
-	CandidateEvent, CandidateHash, CommittedCandidateReceipt, CoreState, DisputeState,
-	ExecutorParams, GroupRotationInfo, Hash, Id as ParaId, InboundDownwardMessage,
-	InboundHrmpMessage, Moment, Nonce, OccupiedCoreAssumption, PersistedValidationData,
-	ScrapedOnChainVotes, SessionInfo, Signature, ValidationCode, ValidationCodeHash, ValidatorId,
-	ValidatorIndex, LOWEST_PUBLIC_ID, PARACHAIN_KEY_TYPE_ID,
+	slashing,
+	vstaging::{ApprovalVotingParams, NodeFeatures},
+	AccountId, AccountIndex, Balance, BlockNumber, CandidateEvent, CandidateHash,
+	CommittedCandidateReceipt, CoreState, DisputeState, ExecutorParams, GroupRotationInfo, Hash,
+	Id as ParaId, InboundDownwardMessage, InboundHrmpMessage, Moment, Nonce,
+	OccupiedCoreAssumption, PersistedValidationData, ScrapedOnChainVotes, SessionInfo, Signature,
+	ValidationCode, ValidationCodeHash, ValidatorId, ValidatorIndex, LOWEST_PUBLIC_ID,
+	PARACHAIN_KEY_TYPE_ID,
 };
 use runtime_common::{
 	auctions, claims, crowdloan, impl_runtime_weights,
@@ -1899,7 +1901,7 @@ sp_api::impl_runtime_apis! {
 		}
 	}
 
-	#[api_version(9)]
+	#[api_version(10)]
 	impl primitives::runtime_api::ParachainHost<Block> for Runtime {
 		fn validators() -> Vec<ValidatorId> {
 			parachains_runtime_api_impl::validators::<Runtime>()
@@ -2049,6 +2051,10 @@ sp_api::impl_runtime_apis! {
 
 		fn node_features() -> NodeFeatures {
 			parachains_vstaging_api_impl::node_features::<Runtime>()
+		}
+
+		fn approval_voting_params() -> ApprovalVotingParams {
+			parachains_vstaging_api_impl::approval_voting_params::<Runtime>()
 		}
 	}
 
