f3b78d181cb3251fdb760276c22ff4bbada17785
muharem <ismailov.m.h@gmail.com>
2024-03-05 18:40:21 +0100

    bridge asset transfer tests

--- integration-tests/emulated/tests/bridges/bridge-hub-polkadot/src/lib.rs
+++ integration-tests/emulated/tests/bridges/bridge-hub-polkadot/src/lib.rs
@@ -33,6 +33,7 @@ pub use bp_messages::LaneId;
 // Cumulus
 pub use emulated_integration_tests_common::{
 	accounts::ALICE,
+	accounts::BOB,
 	impls::Inspect,
 	xcm_emulator::{
 		assert_expected_events, bx, helpers::weight_within_threshold, Chain, Parachain as Para,
