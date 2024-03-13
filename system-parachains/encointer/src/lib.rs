31c24ebcb74b9a7ddf59ab50f381108d27cbf7d6
Alain Brenzikofer <alain@integritee.network>
2024-03-13 13:29:28 +0100

    increase meetup size target from 10 to 15

--- system-parachains/encointer/src/lib.rs
+++ system-parachains/encointer/src/lib.rs
@@ -510,7 +510,7 @@ parameter_types! {
 	pub const MomentsPerDay: Moment = 86_400_000; // [ms/d]
 	pub const DefaultDemurrage: Demurrage = Demurrage::from_bits(0x0000000000000000000001E3F0A8A973_i128);
 	pub const EncointerExistentialDeposit: BalanceType = BalanceType::from_bits(0x0000000000000000000053e2d6238da4_u128);
-	pub const MeetupSizeTarget: u64 = 10;
+	pub const MeetupSizeTarget: u64 = 15;
 	pub const MeetupMinSize: u64 = 3;
 	pub const MeetupNewbieLimitDivider: u64 = 2; // 2 means 1/3 of participants may be newbies
 	pub const FaucetPalletId: PalletId = PalletId(*b"ectrfct0");
