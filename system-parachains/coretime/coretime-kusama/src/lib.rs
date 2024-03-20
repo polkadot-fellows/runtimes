cc858b26094d5f419af6eb09b567f53ddc292e76
Dónal Murray <donal.murray@parity.io>
2024-03-20 22:12:56 +0700

    Remove fast-runtime binary reference

--- system-parachains/coretime/coretime-kusama/src/lib.rs
+++ system-parachains/coretime/coretime-kusama/src/lib.rs
@@ -21,14 +21,6 @@
 #[cfg(feature = "std")]
 include!(concat!(env!("OUT_DIR"), "/wasm_binary.rs"));
 
-/// Provides the `WASM_BINARY` build with `fast-runtime` feature enabled.
-///
-/// This is for example useful for local test chains.
-#[cfg(feature = "std")]
-pub mod fast_runtime_binary {
-	include!(concat!(env!("OUT_DIR"), "/fast_runtime_binary.rs"));
-}
-
 mod coretime;
 #[cfg(test)]
 mod tests;
