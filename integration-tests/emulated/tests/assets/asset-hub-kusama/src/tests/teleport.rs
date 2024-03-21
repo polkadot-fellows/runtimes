4637819b95b9ead14458f0f5d433ff656f056a7c
muharem <ismailov.m.h@gmail.com>
2024-03-21 16:58:13 +0100

    collectives teleport tests

--- integration-tests/emulated/tests/assets/asset-hub-kusama/src/tests/teleport.rs
+++ integration-tests/emulated/tests/assets/asset-hub-kusama/src/tests/teleport.rs
@@ -546,7 +546,7 @@ fn teleport_to_other_system_parachains_works() {
 	let amount = ASSET_HUB_KUSAMA_ED * 100;
 	let native_asset: Assets = (Parent, amount).into();
 
-	test_parachain_is_trusted_teleporter!(
+	test_sibling_is_trusted_teleporter!(
 		AssetHubKusama,          // Origin
 		AssetHubKusamaXcmConfig, // XCM Configuration
 		vec![BridgeHubKusama],   // Destinations
