7a3acb4c656c38d8e2cba1dc6bd124ff8897b132
Svyatoslav Nikolsky <svyatonik@gmail.com>
2024-04-19 17:16:13 +0300

    Update system-parachains/bridge-hubs/bridge-hub-polkadot/src/lib.rs
    Co-authored-by: Adrian Catangiu <adrian@parity.io>

--- system-parachains/bridge-hubs/bridge-hub-polkadot/src/lib.rs
+++ system-parachains/bridge-hubs/bridge-hub-polkadot/src/lib.rs
@@ -169,7 +169,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("bridge-hub-polkadot"),
 	impl_name: create_runtime_str!("bridge-hub-polkadot"),
 	authoring_version: 1,
-	spec_version: 1_002_001,
+	spec_version: 1_002_002,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 3,
