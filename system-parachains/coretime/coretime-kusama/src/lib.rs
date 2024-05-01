f31de907870d6018052c4a2a90993607139dfd06
joepetrowski <joe@parity.io>
2024-05-01 17:04:24 +0200

    remove ImportLeases

--- system-parachains/coretime/coretime-kusama/src/lib.rs
+++ system-parachains/coretime/coretime-kusama/src/lib.rs
@@ -109,7 +109,6 @@ pub type UncheckedExtrinsic =
 /// Migrations to apply on runtime upgrade.
 pub type Migrations = (
 	pallet_xcm::migration::MigrateToLatestXcmVersion<Runtime>,
-	migrations::bootstrapping::ImportLeases,
 	pallet_collator_selection::migration::v2::MigrationToV2<Runtime>,
 	migrations::bootstrapping::RemoveOutdatedPoolAssignment,
 	migrations::bootstrapping::OnboardPeople,
