eccc4f9e4ffa35f978919d6af4ba64129eb8f77e
Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
2024-05-02 18:14:13 +0200

    Allow unused migration
    Signed-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>

--- system-parachains/encointer/src/migrations_fix.rs
+++ system-parachains/encointer/src/migrations_fix.rs
@@ -144,6 +144,7 @@ pub mod scheduler {
 			StorageMap<Pallet<T>, Twox64Concat, TaskName, TaskAddress<BlockNumberFor<T>>>;
 
 		/// Migrate the scheduler pallet from V0 to V4 by brute-force emptying the agenda.
+		#[allow(dead_code)]
 		pub struct MigrateToV4<T>(sp_std::marker::PhantomData<T>);
 
 		impl<T: Config> OnRuntimeUpgrade for MigrateToV4<T> {
