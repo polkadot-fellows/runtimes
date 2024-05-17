b13f7a97601e91759ebd8c5aabfb3a334f03b418
claravanstaden <claravanstaden64@gmail.com>
2024-05-17 16:07:46 +0200

    adds migration

--- system-parachains/bridge-hubs/bridge-hub-polkadot/src/lib.rs
+++ system-parachains/bridge-hubs/bridge-hub-polkadot/src/lib.rs
@@ -24,6 +24,7 @@ include!(concat!(env!("OUT_DIR"), "/wasm_binary.rs"));
 
 pub mod bridge_to_ethereum_config;
 pub mod bridge_to_kusama_config;
+mod bridge_to_ethereum_unstuck;
 mod bridge_to_kusama_unstuck;
 mod weights;
 pub mod xcm_config;
@@ -149,6 +150,7 @@ pub type Migrations = (
 		ConstU32<ASSET_HUB_ID>,
 	>,
 	bridge_to_kusama_unstuck::BridgeToKusamaUnstuck,
+	bridge_to_ethereum_unstuck::UnstuckSnowbridge,
 	pallet_collator_selection::migration::v2::MigrationToV2<Runtime>,
 	// permanent
 	pallet_xcm::migration::MigrateToLatestXcmVersion<Runtime>,
