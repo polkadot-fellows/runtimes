f5472d229e1c6d78d8ec11463ed58eea4d8dd374
claravanstaden <claravanstaden64@gmail.com>
2024-05-17 20:21:46 +0200

    fmt

--- system-parachains/bridge-hubs/bridge-hub-polkadot/src/bridge_to_ethereum_unstuck.rs
+++ system-parachains/bridge-hubs/bridge-hub-polkadot/src/bridge_to_ethereum_unstuck.rs
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Cumulus.  If not, see <http://www.gnu.org/licenses/>.
 
-use crate::{weights::RocksDbWeight, Runtime, weights};
+use crate::{weights, weights::RocksDbWeight, Runtime};
 use codec::Decode;
 use frame_support::{traits::OnRuntimeUpgrade, weights::Weight};
 use hex_literal::hex;
