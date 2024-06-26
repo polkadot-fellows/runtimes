e3444e7ea5fa403eb5b950d25251373aa1b8e7ff
Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
2024-06-26 22:02:20 +0200

    Add scheduler params
    Signed-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>

--- chain-spec-generator/src/relay_chain_specs.rs
+++ chain-spec-generator/src/relay_chain_specs.rs
@@ -74,6 +74,11 @@ fn default_parachains_host_configuration() -> HostConfiguration<polkadot_primiti
 		relay_vrf_modulo_samples: 2,
 		zeroth_delay_tranche_width: 0,
 		minimum_validation_upgrade_delay: 5,
+		scheduler_params: polkadot_primitives::vstaging::SchedulerParams {
+			group_rotation_frequency: 20,
+			paras_availability_period: 4,
+			..Default::default()
+		},
 		..Default::default()
 	}
 }
