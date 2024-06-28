22a26585b51777ecf119a6c96cb4cdcebea3eac9
claravanstaden <claravanstaden64@gmail.com>
2024-06-28 15:42:43 +0200

    fix benchmarks

--- system-parachains/bridge-hubs/bridge-hub-polkadot/src/bridge_to_ethereum_config.rs
+++ system-parachains/bridge-hubs/bridge-hub-polkadot/src/bridge_to_ethereum_config.rs
@@ -166,14 +166,14 @@ pub mod benchmark_helpers {
 	use codec::Encode;
 	use frame_support::traits::fungible;
 	use hex_literal::hex;
-	use snowbridge_beacon_primitives::CompactExecutionHeader;
+	use snowbridge_beacon_primitives::BeaconHeader;
 	use snowbridge_pallet_inbound_queue::BenchmarkHelper;
 	use sp_core::H256;
 	use xcm::latest::{Assets, Location, SendError, SendResult, SendXcm, Xcm, XcmHash};
 
 	impl<T: snowbridge_pallet_ethereum_client::Config> BenchmarkHelper<T> for Runtime {
-		fn initialize_storage(block_hash: H256, header: CompactExecutionHeader) {
-			EthereumBeaconClient::store_execution_header(block_hash, header, 0, H256::default());
+		fn initialize_storage(beacon_header: BeaconHeader, block_roots_root: H256) {
+			EthereumBeaconClient::store_finalized_header(beacon_header, block_roots_root).unwrap();
 			EthereumGatewayAddress::set(&hex!["EDa338E4dC46038493b885327842fD3E301CaB39"].into());
 		}
 	}
