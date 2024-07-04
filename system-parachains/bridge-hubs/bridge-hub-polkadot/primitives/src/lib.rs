57121fd2890390d795a15ce264c1c70c55099224
Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
2024-07-04 23:03:43 +0200

    Update system-parachains/bridge-hubs/bridge-hub-polkadot/primitives/src/lib.rs
    Co-authored-by: Adrian Catangiu <adrian@parity.io>

--- system-parachains/bridge-hubs/bridge-hub-polkadot/primitives/src/lib.rs
+++ system-parachains/bridge-hubs/bridge-hub-polkadot/primitives/src/lib.rs
@@ -58,7 +58,7 @@ impl Chain for BridgeHubPolkadot {
 
 impl Parachain for BridgeHubPolkadot {
 	const PARACHAIN_ID: u32 = BRIDGE_HUB_POLKADOT_PARACHAIN_ID;
-	const MAX_HEADER_SIZE: u32 = MAX_BRIDGE_HUB_HEADER_SIZE; // FAIL-CI @svyatonik please check
+	const MAX_HEADER_SIZE: u32 = MAX_BRIDGE_HUB_HEADER_SIZE;
 }
 
 impl ChainWithMessages for BridgeHubPolkadot {
