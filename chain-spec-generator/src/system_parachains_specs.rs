255e6702f547a2dda4e08cae69e9b0b547532924
Branislav Kontur <bkontur@gmail.com>
2024-07-16 15:55:21 +0200

    Set `get_preset` for CollectivesPolkadot

--- chain-spec-generator/src/system_parachains_specs.rs
+++ chain-spec-generator/src/system_parachains_specs.rs
@@ -56,8 +56,6 @@ const ASSET_HUB_POLKADOT_ED: Balance = asset_hub_polkadot_runtime::ExistentialDe
 
 const ASSET_HUB_KUSAMA_ED: Balance = asset_hub_kusama_runtime::ExistentialDeposit::get();
 
-const COLLECTIVES_POLKADOT_ED: Balance = collectives_polkadot_runtime::ExistentialDeposit::get();
-
 const ENCOINTER_KUSAMA_ED: Balance = encointer_kusama_runtime::ExistentialDeposit::get();
 
 const CORETIME_KUSAMA_ED: Balance = coretime_kusama_runtime::ExistentialDeposit::get();
@@ -112,15 +110,6 @@ pub fn asset_hub_polkadot_session_keys(
 /// The input must be a tuple of individual keys (a single arg for now since we have just one key).
 pub fn asset_hub_kusama_session_keys(keys: AuraId) -> asset_hub_kusama_runtime::SessionKeys {
 	asset_hub_kusama_runtime::SessionKeys { aura: keys }
-}
-
-/// Generate the session keys from individual elements.
-///
-/// The input must be a tuple of individual keys (a single arg for now since we have just one key).
-pub fn collectives_polkadot_session_keys(
-	keys: AuraId,
-) -> collectives_polkadot_runtime::SessionKeys {
-	collectives_polkadot_runtime::SessionKeys { aura: keys }
 }
 
 /// Generate the session keys from individual elements.
@@ -281,58 +270,6 @@ pub fn asset_hub_kusama_local_testnet_config() -> Result<Box<dyn ChainSpec>, Str
 	))
 }
 
-// CollectivesPolkadot
-fn collectives_polkadot_genesis(
-	invulnerables: Vec<(AccountId, AuraId)>,
-	endowed_accounts: Vec<AccountId>,
-	id: ParaId,
-) -> serde_json::Value {
-	serde_json::json!({
-		"balances": collectives_polkadot_runtime::BalancesConfig {
-			balances: endowed_accounts
-				.iter()
-				.cloned()
-				.map(|k| (k, COLLECTIVES_POLKADOT_ED * 4096 * 4096))
-				.collect(),
-		},
-		"parachainInfo": collectives_polkadot_runtime::ParachainInfoConfig {
-			parachain_id: id,
-			..Default::default()
-		},
-		"collatorSelection": collectives_polkadot_runtime::CollatorSelectionConfig {
-			invulnerables: invulnerables.iter().cloned().map(|(acc, _)| acc).collect(),
-			candidacy_bond: COLLECTIVES_POLKADOT_ED * 16,
-			..Default::default()
-		},
-		"session": collectives_polkadot_runtime::SessionConfig {
-			keys: invulnerables
-				.into_iter()
-				.map(|(acc, aura)| {
-					(
-						acc.clone(),                             // account id
-						acc,                                     // validator id
-						collectives_polkadot_session_keys(aura), // session keys
-					)
-				})
-				.collect(),
-		},
-		"polkadotXcm": {
-			"safeXcmVersion": Some(SAFE_XCM_VERSION),
-		},
-		// no need to pass anything to aura, in fact it will panic if we do. Session will take care
-		// of this. `aura: Default::default()`
-	})
-}
-
-fn collectives_polkadot_local_genesis(para_id: ParaId) -> serde_json::Value {
-	collectives_polkadot_genesis(
-		// initial collators.
-		invulnerables(),
-		testnet_accounts(),
-		para_id,
-	)
-}
-
 pub fn collectives_polkadot_local_testnet_config() -> Result<Box<dyn ChainSpec>, String> {
 	let mut properties = sc_chain_spec::Properties::new();
 	properties.insert("ss58Format".into(), 0.into());
@@ -348,7 +285,7 @@ pub fn collectives_polkadot_local_testnet_config() -> Result<Box<dyn ChainSpec>,
 		.with_name("Polkadot Collectives Local")
 		.with_id("collectives-polkadot-local")
 		.with_chain_type(ChainType::Local)
-		.with_genesis_config_patch(collectives_polkadot_local_genesis(1001.into()))
+		.with_genesis_config_patch(collectives_polkadot_runtime::genesis_config_presets::collectives_polkadot_local_testnet_genesis(1001.into()))
 		.with_properties(properties)
 		.build(),
 	))
