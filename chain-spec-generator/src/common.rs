afe696b390e7cbe88b54dec69aeee1322fdea540
Branislav Kontur <bkontur@gmail.com>
2024-07-25 00:44:37 +0200

    Short benchmarking in CI with `frame-omni-bencher` + extract `chain-spec-builder` stuff to `get_preset` (#379)
    This PR introduces a new CI pipeline for checking and running runtime
    benchmarks as part of the test pipelines. This means we will now know if
    any changes break the benchmarks.
    
    The pipeline is based on downloading `frame-omni-bencher` (building it
    in-place is too expensive for every matrix run) and running it with a
    minimal setup: `--steps 2 --repeat 1`.
    
    Another part of this PR splits the default genesis setups from
    `chain-spec-generator` and moves them to the
    `sp_genesis_builder::GenesisBuilder::get_preset` runtime API
    implementation for every runtime, which is required by
    `frame-omni-bencher`.
    
    
    Closes: https://github.com/polkadot-fellows/runtimes/issues/197
    Relates to: https://github.com/polkadot-fellows/runtimes/pull/298
    Relates to: https://github.com/polkadot-fellows/runtimes/pull/324
    
    <!-- Remember that you can run `/merge` to enable auto-merge in the PR
    -->
    
    <!-- Remember to modify the changelog. If you don't need to modify it,
    you can check the following box.
    Instead, if you have already modified it, simply delete the following
    line. -->
    
    - [X] Does not require a CHANGELOG entry
    
    ## Future works
    
    When [this issue](https://github.com/paritytech/polkadot-sdk/pull/5083)
    is fixed, I will rewrite
    [weight-generation.md](https://github.com/polkadot-fellows/runtimes/blob/main/docs/weight-generation.md).
    The new version will be significantly simplified, with instructions to
    simply download `frame-omni-bencher`, build the runtime WASM, and run
    it—no other steps will be necessary.
    
    ```
    2024-07-18 14:45:51 Using the chain spec instead of the runtime to generate the genesis state is deprecated. Please remove the `--chain`/`--dev`/`--local` argument, point `--runtime` to your runtime blob and set `--genesis-builder=runtime`. This warning may become a hard error any time after December 2024.    
    ```
    
    ---------
    
    Co-authored-by: Bastian Köcher <git@kchr.de>

--- chain-spec-generator/src/common.rs
+++ chain-spec-generator/src/common.rs
@@ -19,46 +19,12 @@ use crate::{
 	relay_chain_specs::{KusamaChainSpec, PolkadotChainSpec},
 	system_parachains_specs::{
 		AssetHubKusamaChainSpec, AssetHubPolkadotChainSpec, BridgeHubKusamaChainSpec,
-		BridgeHubPolkadotChainSpec, CollectivesPolkadotChainSpec, EncointerKusamaChainSpec,
-		GluttonKusamaChainSpec, PeopleKusamaChainSpec, PeoplePolkadotChainSpec,
+		BridgeHubPolkadotChainSpec, CollectivesPolkadotChainSpec, CoretimeKusamaChainSpec,
+		EncointerKusamaChainSpec, GluttonKusamaChainSpec, PeopleKusamaChainSpec,
+		PeoplePolkadotChainSpec,
 	},
 	ChainSpec,
 };
-use polkadot_primitives::{AccountId, AccountPublic};
-use sp_core::{sr25519, Pair, Public};
-use sp_runtime::traits::IdentifyAccount;
-
-pub fn testnet_accounts() -> Vec<AccountId> {
-	vec![
-		get_account_id_from_seed::<sr25519::Public>("Alice"),
-		get_account_id_from_seed::<sr25519::Public>("Bob"),
-		get_account_id_from_seed::<sr25519::Public>("Charlie"),
-		get_account_id_from_seed::<sr25519::Public>("Dave"),
-		get_account_id_from_seed::<sr25519::Public>("Eve"),
-		get_account_id_from_seed::<sr25519::Public>("Ferdie"),
-		get_account_id_from_seed::<sr25519::Public>("Alice//stash"),
-		get_account_id_from_seed::<sr25519::Public>("Bob//stash"),
-		get_account_id_from_seed::<sr25519::Public>("Charlie//stash"),
-		get_account_id_from_seed::<sr25519::Public>("Dave//stash"),
-		get_account_id_from_seed::<sr25519::Public>("Eve//stash"),
-		get_account_id_from_seed::<sr25519::Public>("Ferdie//stash"),
-	]
-}
-
-/// Helper function to generate a crypto pair from seed
-pub fn get_from_seed<TPublic: Public>(seed: &str) -> <TPublic::Pair as Pair>::Public {
-	TPublic::Pair::from_string(&format!("//{}", seed), None)
-		.expect("static values are valid; qed")
-		.public()
-}
-
-/// Helper function to generate an account ID from seed
-pub fn get_account_id_from_seed<TPublic: Public>(seed: &str) -> AccountId
-where
-	AccountPublic: From<<TPublic::Pair as Pair>::Public>,
-{
-	AccountPublic::from(get_from_seed::<TPublic>(seed)).into_account()
-}
 
 #[derive(Debug, serde::Deserialize)]
 struct EmptyChainSpecWithId {
@@ -86,6 +52,8 @@ pub fn from_json_file(filepath: &str, supported: String) -> Result<Box<dyn Chain
 			Ok(Box::new(BridgeHubPolkadotChainSpec::from_json_file(path)?)),
 		x if x.starts_with("bridge-hub-kusama") =>
 			Ok(Box::new(BridgeHubKusamaChainSpec::from_json_file(path)?)),
+		x if x.starts_with("coretime-kusama") =>
+			Ok(Box::new(CoretimeKusamaChainSpec::from_json_file(path)?)),
 		x if x.starts_with("glutton-kusama") =>
 			Ok(Box::new(GluttonKusamaChainSpec::from_json_file(path)?)),
 		x if x.starts_with("encointer-kusama") =>
