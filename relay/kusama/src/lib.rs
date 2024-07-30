0a53150b9bb3853fa57c5f5d836a0ba7cd73e7ac
Tsvetomir Dimitrov <tsvetomir@parity.io>
2024-07-30 17:03:23 +0300

    fmt

--- relay/kusama/src/lib.rs
+++ relay/kusama/src/lib.rs
@@ -1115,7 +1115,8 @@ impl InstanceFilter<RuntimeCall> for ProxyType {
 				matches!(
 					c,
 					RuntimeCall::Staking(..) |
-						RuntimeCall::Session(..) | RuntimeCall::Utility(..) |
+						RuntimeCall::Session(..) |
+						RuntimeCall::Utility(..) |
 						RuntimeCall::FastUnstake(..) |
 						RuntimeCall::VoterList(..) |
 						RuntimeCall::NominationPools(..)
