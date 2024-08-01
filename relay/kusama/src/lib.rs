28adc6bcbea80dc4a3dcd067fa72591abded7f7c
kianenigma <kian@parity.io>
2024-08-01 11:28:06 +0100

    fmt

--- relay/kusama/src/lib.rs
+++ relay/kusama/src/lib.rs
@@ -1268,8 +1268,7 @@ impl InstanceFilter<RuntimeCall> for ProxyType {
 				matches!(
 					c,
 					RuntimeCall::Staking(..) |
-						RuntimeCall::Session(..) |
-						RuntimeCall::Utility(..) |
+						RuntimeCall::Session(..) | RuntimeCall::Utility(..) |
 						RuntimeCall::FastUnstake(..) |
 						RuntimeCall::VoterList(..) |
 						RuntimeCall::NominationPools(..)
