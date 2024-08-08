9e13618bcb60f11955cd9ff943ab4febf90ed35c
Dónal Murray <donal.murray@parity.io>
2024-08-08 09:53:02 +0100

    Add LocationToAccountAPI to coretime-polkadot

--- system-parachains/coretime/coretime-polkadot/src/lib.rs
+++ system-parachains/coretime/coretime-polkadot/src/lib.rs
@@ -1078,6 +1078,18 @@ impl_runtime_apis! {
 		}
 	}
 
+	impl xcm_runtime_apis::conversions::LocationToAccountApi<Block, AccountId> for Runtime {
+		fn convert_location(location: VersionedLocation) -> Result<
+			AccountId,
+			xcm_runtime_apis::conversions::Error
+		> {
+			xcm_runtime_apis::conversions::LocationToAccountHelper::<
+				AccountId,
+				xcm_config::LocationToAccountId,
+			>::convert_location(location)
+		}
+	}
+
 	impl sp_genesis_builder::GenesisBuilder<Block> for Runtime {
 		fn build_state(config: Vec<u8>) -> sp_genesis_builder::Result {
 			build_state::<RuntimeGenesisConfig>(config)
