7157d41176bebf128aa2e29e72ed184844446b19
Muharem <ismailov.m.h@gmail.com>
2024-08-26 00:27:41 +0200

    Utilize SDK types for native asset balance conversion (#424)
    Utilize SDK types for native asset balance conversion in the Treasury
    pallet setup.
    
    This change does not introduce any behavioral changes; it simply updates
    the code to use common types from the SDK.
    
    - [x] Does not require a CHANGELOG entry
    
    ---------
    
    Co-authored-by: Bastian Köcher <git@kchr.de>
    Co-authored-by: fellowship-merge-bot[bot] <151052383+fellowship-merge-bot[bot]@users.noreply.github.com>

--- relay/polkadot/src/impls.rs
+++ relay/polkadot/src/impls.rs
@@ -14,53 +14,9 @@
 // You should have received a copy of the GNU General Public License
 // along with Polkadot.  If not, see <http://www.gnu.org/licenses/>.
 
-use super::*;
-use core::marker::PhantomData;
-use frame_support::traits::{tokens::ConversionFromAssetBalance, Contains};
-use polkadot_primitives::Id as ParaId;
-use xcm_builder::IsChildSystemParachain;
-
-// TODO: replace by types from polkadot-sdk https://github.com/paritytech/polkadot-sdk/pull/3659
-/// Determines if the given `asset_kind` is a native asset. If it is, returns the balance without
-/// conversion; otherwise, delegates to the implementation specified by `I`.
-///
-/// Example where the `asset_kind` represents the native asset:
-/// - location: (1, Parachain(1000)), // location of a Sibling Parachain;
-/// - asset_id: (1, Here), // the asset id in the context of `asset_kind.location`;
-pub struct NativeOnSystemParachain<I>(PhantomData<I>);
-impl<I> ConversionFromAssetBalance<Balance, VersionedLocatableAsset, Balance>
-	for NativeOnSystemParachain<I>
-where
-	I: ConversionFromAssetBalance<Balance, VersionedLocatableAsset, Balance>,
-{
-	type Error = ();
-	fn from_asset_balance(
-		balance: Balance,
-		asset_kind: VersionedLocatableAsset,
-	) -> Result<Balance, Self::Error> {
-		use VersionedLocatableAsset::*;
-		let (location, asset_id) = match asset_kind.clone() {
-			V3 { location, asset_id } => (location.try_into()?, asset_id.try_into()?),
-			V4 { location, asset_id } => (location, asset_id),
-		};
-		if asset_id.0.contains_parents_only(1) &&
-			IsChildSystemParachain::<ParaId>::contains(&location)
-		{
-			Ok(balance)
-		} else {
-			I::from_asset_balance(balance, asset_kind).map_err(|_| ())
-		}
-	}
-	#[cfg(feature = "runtime-benchmarks")]
-	fn ensure_successful(asset_kind: VersionedLocatableAsset) {
-		I::ensure_successful(asset_kind)
-	}
-}
-
 #[cfg(feature = "runtime-benchmarks")]
 pub mod benchmarks {
-	use super::{xcm_config::CheckAccount, ExistentialDeposit};
-	use crate::Balances;
+	use crate::{xcm_config::CheckAccount, Balances, ExistentialDeposit};
 	use frame_support::{
 		dispatch::RawOrigin,
 		traits::{Currency, EnsureOrigin},
