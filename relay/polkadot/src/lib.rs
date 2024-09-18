9d486d991c8e29f33f571a5e883cd171d5759c7f
kianenigma <kian@parity.io>
2024-09-18 11:13:34 +0100

    nit

--- relay/polkadot/src/lib.rs
+++ relay/polkadot/src/lib.rs
@@ -2147,7 +2147,7 @@ impl Runtime {
 				.into_iter()
 				// all active para-ids that do not belong to a system chain is the number of
 				// parachains that we should take into account for inflation.
-				.filter(|i| *i >= 2000.into())
+				.filter(|i| *i >= LOWEST_PUBLIC_ID)
 				.count() as u64;
 			ideal_staking_rate
 				.saturating_sub(Perquintill::from_rational(auctioned_slots.min(60), 300u64))
