d98f0e65fe8ecdac58f8bbc9d90fc289a23594c8
eskimor <eskimor@no-such-url.com>
2024-09-29 12:43:34 +0200

    fmt

--- system-parachains/coretime/coretime-polkadot/src/migrations.rs
+++ system-parachains/coretime/coretime-polkadot/src/migrations.rs
@@ -25,12 +25,16 @@ extern crate alloc;
 use crate::{weights, Runtime, RuntimeOrigin};
 use frame_support::{pallet_prelude::*, traits::OnRuntimeUpgrade};
 use pallet_broker::{
-	CompletionStatus, CoreAssignment::{self, Pool}, CoreIndex, CoreMask, LeaseRecordItem, Leases, LeasesRecordOf, PotentialRenewalId, PotentialRenewals, SalePerformance, Schedule, ScheduleItem, TaskId, Timeslice, WeightInfo, Workplan, AdaptPrice,
+	AdaptPrice, CompletionStatus,
+	CoreAssignment::{self, Pool},
+	CoreIndex, CoreMask, LeaseRecordItem, Leases, LeasesRecordOf, PotentialRenewalId,
+	PotentialRenewals, SalePerformance, Schedule, ScheduleItem, TaskId, Timeslice, WeightInfo,
+	Workplan,
 };
 
-use sp_std::vec::Vec;
 use alloc::collections::btree_map::BTreeMap;
 use sp_arithmetic::traits::Saturating;
+use sp_std::vec::Vec;
 
 #[cfg(feature = "try-runtime")]
 use pallet_broker::{CoreAssignment::Task, PotentialRenewalRecord, SaleInfo, SaleInfoRecordOf};
@@ -47,152 +51,157 @@ type BrokerWeights = weights::pallet_broker::WeightInfo<Runtime>;
 pub struct FixMigration;
 
 impl FixMigration {
+	fn get_first_usable_core() -> Option<CoreIndex> {
+		let sale_info = SaleInfo::<Runtime>::get()?;
+		// Cores between first_core and cores_offered are for sale and my not be used anymore:
+		Some(sale_info.first_core + sale_info.cores_offered)
+	}
 
-    fn get_first_usable_core() -> Option<CoreIndex> {
-        let sale_info = SaleInfo::<Runtime>::get()?;
-        // Cores between first_core and cores_offered are for sale and my not be used anymore:
-        Some(sale_info.first_core + sale_info.cores_offered)
-    }
+	fn add_pool_core(first_core: &mut CoreIndex) {
+		let Some(sale_info) = SaleInfo::<Runtime>::get() else {
+			log::error!(target: TARGET, "Retrieving `SaleInfo` failed!");
+			return
+		};
 
-    fn add_pool_core(first_core: &mut CoreIndex) {
-        let Some(sale_info) = SaleInfo::<Runtime>::get() else {
-            log::error!(target: TARGET, "Retrieving `SaleInfo` failed!");
-            return
-        };
+		let schedule = BoundedVec::truncate_from(Vec::from([ScheduleItem {
+			mask: CoreMask::complete(),
+			assignment: CoreAssignment::Pool,
+		}]));
+		let result =
+			pallet_broker::Pallet::<Runtime>::reserve(RuntimeOrigin::root(), schedule.clone());
+		debug_assert!(result.is_ok());
+		// But we want it now(ish): add it to the workplan.
+		Workplan::<Runtime>::insert((sale_info.region_begin, *first_core), schedule);
+		first_core.saturating_inc();
+	}
 
-        let schedule = BoundedVec::truncate_from(Vec::from([ScheduleItem { mask: CoreMask::complete(), assignment: CoreAssignment::Pool}]));
-        let result = pallet_broker::Pallet::<Runtime>::reserve(RuntimeOrigin::root(), schedule.clone());
-        debug_assert!(result.is_ok());
-        // But we want it now(ish): add it to the workplan.
-        Workplan::<Runtime>::insert((sale_info.region_begin, *first_core), schedule);
-        first_core.saturating_inc();
-    }
+	fn extend_short_leases() {
+		let short_leases: BTreeMap<TaskId, Timeslice> = [
+			(2035, 359280),
+			(3344, 344160),
+			(3370, 389520),
+			(3367, 374400),
+			(2086, 389520),
+			(2032, 359280),
+			(2051, 389520),
+			(3369, 313920),
+			(2008, 389520),
+			(2025, 359280),
+			(2000, 313920),
+			(2092, 404640),
+			(2002, 359280),
+			(3359, 389520),
+			(2030, 374400),
+			(3378, 404640),
+			(2104, 404640),
+			(2046, 374400),
+			(3345, 344160),
+			(3340, 344160),
+			(3338, 329040),
+			(2004, 329040),
+			(3377, 389520),
+			(3373, 389520),
+			(2031, 344160),
+			(3389, 404640),
+			(3366, 374400),
+			(2037, 374400),
+			(2034, 359280),
+			(2090, 404640),
+			(3346, 344160),
+			(2012, 359280),
+			(3397, 404640),
+			(2043, 374400),
+			(2091, 404640),
+			(2026, 344160),
+			(3388, 404640),
+			(3354, 359280),
+			(2006, 344160),
+			(2013, 374400),
+		]
+		.into();
 
-    fn extend_short_leases() {
-        let short_leases: BTreeMap<TaskId, Timeslice> = [
-            (2035, 359280),
-            (3344, 344160),
-            (3370, 389520),
-            (3367, 374400),
-            (2086, 389520),
-            (2032, 359280),
-            (2051, 389520),
-            (3369, 313920),
-            (2008, 389520),
-            (2025, 359280),
-            (2000, 313920),
-            (2092, 404640),
-            (2002, 359280),
-            (3359, 389520),
-            (2030, 374400),
-            (3378, 404640),
-            (2104, 404640),
-            (2046, 374400),
-            (3345, 344160),
-            (3340, 344160),
-            (3338, 329040),
-            (2004, 329040),
-            (3377, 389520),
-            (3373, 389520),
-            (2031, 344160),
-            (3389, 404640),
-            (3366, 374400),
-            (2037, 374400),
-            (2034, 359280),
-            (2090, 404640),
-            (3346, 344160),
-            (2012, 359280),
-            (3397, 404640),
-            (2043, 374400),
-            (2091, 404640),
-            (2026, 344160),
-            (3388, 404640),
-            (3354, 359280),
-            (2006, 344160),
-            (2013, 374400),
-        ].into();
+		let leases = Leases::<Runtime>::get();
+		let leases = BoundedVec::truncate_from(
+			leases
+				.into_iter()
+				.map(|mut l| {
+					let Some(new_until) = short_leases.get(&l.task) else { return l };
+					debug_assert!(*new_until > l.until);
+					l.until = *new_until;
+					l
+				})
+				.collect(),
+		);
+		Leases::<Runtime>::put(leases);
+	}
 
-        let leases = Leases::<Runtime>::get();
-        let leases = BoundedVec::truncate_from(leases.into_iter().map(|mut l| {
-            let Some(new_until) = short_leases.get(&l.task) else {
-                return l
-            };
-            debug_assert!(*new_until > l.until);
-            l.until = *new_until;
-            l
-        }).collect());
-        Leases::<Runtime>::put(leases);
-    }
+	// Undo rotate_sale effect on leases that wrongly expired.
+	//
+	// Needs to be called before `give_dropped_leases_renewal_rights_and_workplan_entry`.
+	fn remove_premature_renewals_add_back_leases() {
+		let premature_renewals = [
+			(2094, 298800),
+			(2040, 298800),
+			(3333, 298800),
+			(2106, 298800),
+			(2093, 298800),
+			(2101, 298800),
+		];
 
-    // Undo rotate_sale effect on leases that wrongly expired.
-    //
-    // Needs to be called before `give_dropped_leases_renewal_rights_and_workplan_entry`.
-    fn remove_premature_renewals_add_back_leases() {
-        let premature_renewals = [
-            (2094, 298800),
-            (2040, 298800),
-            (3333, 298800),
-            (2106, 298800),
-            (2093, 298800),
-            (2101, 298800),
-        ];
+		debug_assert_eq!(PotentialRenewals::<Runtime>::iter().count(), premature_renewals.len());
+		let result = PotentialRenewals::<Runtime>::clear(premature_renewals.len() as u32, None);
+		debug_assert!(result.maybe_cursor.is_none());
 
-        debug_assert_eq!(PotentialRenewals::<Runtime>::iter().count(), premature_renewals.len());
-        let result = PotentialRenewals::<Runtime>::clear(premature_renewals.len() as u32, None);
-        debug_assert!(result.maybe_cursor.is_none());
+		for (task, until) in premature_renewals {
+			let result =
+				pallet_broker::Pallet::<Runtime>::set_lease(RuntimeOrigin::root(), task, until);
+			debug_assert!(result.is_ok());
+		}
+	}
 
-        for (task, until) in premature_renewals {
-            let result = pallet_broker::Pallet::<Runtime>::set_lease(RuntimeOrigin::root(), task, until);
-            debug_assert!(result.is_ok());
-        }
-    }
+	fn give_dropped_leases_renewal_rights_and_workplan_entry(first_core: &mut CoreIndex) {
+		let Some(sale_info) = SaleInfo::<Runtime>::get() else {
+			log::error!(target: TARGET, "Retrieving `SaleInfo` failed!");
+			return
+		};
 
-    fn give_dropped_leases_renewal_rights_and_workplan_entry(first_core: &mut CoreIndex) {
-        let Some(sale_info) = SaleInfo::<Runtime>::get() else {
-            log::error!(target: TARGET, "Retrieving `SaleInfo` failed!");
-            return
-        };
+		let dropped_leases =
+			[(2048, 283680), (3375, 283680), (3358, 283680), (2053, 283680), (2056, 283680)];
 
-        let dropped_leases = [
-            (2048, 283680),
-            (3375, 283680),
-            (3358, 283680),
-            (2053, 283680),
-            (2056, 283680),
-        ];
+		// Leases should have been added, but removed again by rotate_sale - replaces with work
+		// plan items + renewal rights.
+		// https://github.com/paritytech/polkadot-sdk/blob/f170af615c0dc413482100892758b236d1fda93b/substrate/frame/broker/src/tick_impls.rs#L212
+		// So we need to:
+		// - Don't change leases, already correct.
+		// - Add work plan entry.
+		// - add to potential renewals
 
-        // Leases should have been added, but removed again by rotate_sale - replaces with work
-        // plan items + renewal rights.
-        // https://github.com/paritytech/polkadot-sdk/blob/f170af615c0dc413482100892758b236d1fda93b/substrate/frame/broker/src/tick_impls.rs#L212
-        // So we need to:
-        // - Don't change leases, already correct.
-        // - Add work plan entry.
-        // - add to potential renewals
+		for (task, _) in dropped_leases {
+			// Workplan
+			let mask = CoreMask::complete();
+			let assignment = CoreAssignment::Task(task);
+			// DONE: enough to start at next rotation?
+			// - Yes all good, assignments are all for next rotation already. Hence relay chain
+			// state is persisted for now.
+			let schedule =
+				BoundedVec::truncate_from(Vec::from([ScheduleItem { mask, assignment }]));
+			Workplan::<Runtime>::insert((sale_info.region_begin, *first_core), schedule.clone());
 
-        for (task, _) in dropped_leases  {
-            // Workplan
-            let mask = CoreMask::complete();
-            let assignment = CoreAssignment::Task(task);
-            // DONE: enough to start at next rotation?
-            // - Yes all good, assignments are all for next rotation already. Hence relay chain
-            // state is persisted for now.
-            let schedule = BoundedVec::truncate_from(Vec::from([ScheduleItem { mask, assignment }]));
-            Workplan::<Runtime>::insert((sale_info.region_begin, *first_core), schedule.clone());
+			// Renewal:
+			let new_prices = <Runtime as pallet_broker::Config>::PriceAdapter::adapt_price(
+				SalePerformance::from_sale(&sale_info),
+			);
+			debug_assert_eq!(new_prices.target_price, 100 * UNITS);
+			let renewal_id = PotentialRenewalId { core: *first_core, when: sale_info.region_end };
+			let record = PotentialRenewalRecord {
+				price: new_prices.target_price,
+				completion: CompletionStatus::Complete(schedule),
+			};
+			PotentialRenewals::<Runtime>::insert(renewal_id, &record);
 
-            // Renewal:
-            let new_prices = <Runtime as pallet_broker::Config>::PriceAdapter::adapt_price(SalePerformance::from_sale(&sale_info));
-            debug_assert_eq!(new_prices.target_price, 100*UNITS);
-            let renewal_id = PotentialRenewalId { core: *first_core, when: sale_info.region_end };
-            let record = PotentialRenewalRecord {
-                price: new_prices.target_price,
-                completion: CompletionStatus::Complete(schedule),
-            };
-            PotentialRenewals::<Runtime>::insert(renewal_id, &record);
-
-            first_core.saturating_inc()
-        }
-
-    }
+			first_core.saturating_inc()
+		}
+	}
 
 	fn on_runtime_upgrade_donal() -> Weight {
 		if PotentialRenewals::<Runtime>::get(PotentialRenewalId { core: 6, when: 292605 }).is_none()
@@ -276,8 +285,8 @@ impl FixMigration {
 
 		// Add the system pool core to the workplan starting now.
 		let now = 287000; // TODO - this needs to be after the cores have just added have been processed, which takes
-				  // two sessions. Currently just a placeholder. This part is probably better as a call to
-				  // assign_core on the relay as part of the referendum instead of here.
+					// two sessions. Currently just a placeholder. This part is probably better as a call to
+					// assign_core on the relay as part of the referendum instead of here.
 		Workplan::<Runtime>::insert((now, 61), pool_assignment);
 
 		// TODO finalise the weights here.
@@ -298,19 +307,17 @@ impl OnRuntimeUpgrade for FixMigration {
 			return <Runtime as frame_system::Config>::DbWeight::get().reads(1);
 		}
 
-        let Some(mut first_core) = Self::get_first_usable_core() else {
-            log::error!(target: TARGET, "Retrieving `SaleInfo` (first_core) failed!");
-            // Return dummy weight. This should really not happen and if it does we have bigger
-            // problems than wrong weights.
-            return <Runtime as frame_system::Config>::DbWeight::get()
-			.writes(50)
-        };
+		let Some(mut first_core) = Self::get_first_usable_core() else {
+			log::error!(target: TARGET, "Retrieving `SaleInfo` (first_core) failed!");
+			// Return dummy weight. This should really not happen and if it does we have bigger
+			// problems than wrong weights.
+			return <Runtime as frame_system::Config>::DbWeight::get().writes(50)
+		};
 
-        Self::add_pool_core(&mut first_core);
-        Self::extend_short_leases();
-        Self::remove_premature_renewals_add_back_leases();
-        Self::give_dropped_leases_renewal_rights_and_workplan_entry(&mut first_core);
-
+		Self::add_pool_core(&mut first_core);
+		Self::extend_short_leases();
+		Self::remove_premature_renewals_add_back_leases();
+		Self::give_dropped_leases_renewal_rights_and_workplan_entry(&mut first_core);
 
 		// TODO finalise the weights here.
 		<Runtime as frame_system::Config>::DbWeight::get()
