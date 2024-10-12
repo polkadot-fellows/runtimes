f725e7a2e3bb179b57c9a22cae04f22dd2c7a308
dharjeezy <dharjeezy@gmail.com>
2024-10-12 21:35:15 +0100

    bring back remove_phantom_fields for now

--- system-parachains/people/people-polkadot/src/genesis_config_presets.rs
+++ system-parachains/people/people-polkadot/src/genesis_config_presets.rs
@@ -67,7 +67,10 @@ fn people_polkadot_genesis(
 		transaction_payment: Default::default(),
 	};
 
-	serde_json::to_value(config).expect("Could not build genesis config.")
+	let mut config_values = serde_json::to_value(config).expect("Could not build genesis config.");
+	remove_phantom_fields(&mut config_values);
+
+	config_values
 }
 
 pub fn people_polkadot_local_testnet_genesis(para_id: ParaId) -> serde_json::Value {
