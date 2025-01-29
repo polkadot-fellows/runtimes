be6eb4559a38bb0e052fd2d522a14a10116dba15
Bastian Köcher <info@kchr.de>
2025-01-29 21:26:28 +0100

    Fix warning

--- system-parachains/asset-hubs/asset-hub-polkadot/src/lib.rs
+++ system-parachains/asset-hubs/asset-hub-polkadot/src/lib.rs
@@ -77,7 +77,7 @@ use sp_api::impl_runtime_apis;
 use sp_core::{crypto::KeyTypeId, ConstU128, OpaqueMetadata};
 use sp_runtime::{
 	create_runtime_str, generic, impl_opaque_keys,
-	traits::{AccountIdLookup, BlakeTwo256, Block as BlockT, Convert, ConvertInto, Verify},
+	traits::{AccountIdLookup, BlakeTwo256, Block as BlockT, ConvertInto, Verify},
 	transaction_validity::{TransactionSource, TransactionValidity},
 	ApplyExtrinsicResult, Perbill, Permill,
 };
