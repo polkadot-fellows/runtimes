b0016398a9d8258b5dc63da18f58375ad631545a
Nikolai Kozlov <1431590+nkpar@users.noreply.github.com>
2025-03-06 15:02:05 +0000

    Update system-parachains/asset-hubs/asset-hub-polkadot/src/lib.rs
    Co-authored-by: Kian Paimani <5588131+kianenigma@users.noreply.github.com>

--- system-parachains/asset-hubs/asset-hub-polkadot/src/lib.rs
+++ system-parachains/asset-hubs/asset-hub-polkadot/src/lib.rs
@@ -1772,7 +1772,7 @@ impl pallet_state_trie_migration::Config for Runtime {
 	type RuntimeHoldReason = RuntimeHoldReason;
 	type SignedDepositPerItem = MigrationSignedDepositPerItem;
 	type SignedDepositBase = MigrationSignedDepositBase;
-	/// An origin that can control the whole pallet: should be Root, or a part of your council.
+	// An origin that can control the whole pallet: should be Root, or the fellowship.
 	type ControlOrigin = EitherOfDiverse<
 		EnsureRoot<AccountId>,
 		EnsureXcm<IsVoiceOfBody<FellowshipLocation, FellowsBodyId>>,
