974e7cdd70c81f0d13c093389e9d6bfe0166dbb8
muharem <ismailov.m.h@gmail.com>
2025-04-02 13:58:47 +0200

    enable async backing for asset hub kusama

--- system-parachains/asset-hubs/asset-hub-kusama/src/lib.rs
+++ system-parachains/asset-hubs/asset-hub-kusama/src/lib.rs
@@ -81,10 +81,14 @@ use parachains_common::{
 	Header, Nonce, Signature,
 };
 use sp_runtime::RuntimeDebug;
-pub use system_parachains_constants::SLOT_DURATION;
+pub use system_parachains_constants::fast::SLOT_DURATION;
 use system_parachains_constants::{
-	kusama::{consensus::*, currency::*, fee::WeightToFee},
-	AVERAGE_ON_INITIALIZE_RATIO, DAYS, HOURS, MAXIMUM_BLOCK_WEIGHT, NORMAL_DISPATCH_RATIO,
+	fast::{AVERAGE_ON_INITIALIZE_RATIO, DAYS, HOURS, MAXIMUM_BLOCK_WEIGHT, NORMAL_DISPATCH_RATIO},
+	kusama::{
+		consensus::{BLOCK_PROCESSING_VELOCITY, RELAY_CHAIN_SLOT_DURATION_MILLIS},
+		currency::*,
+		fee::WeightToFee,
+	},
 };
 use xcm::{
 	latest::prelude::{AssetId, BodyId},
@@ -205,7 +209,7 @@ impl pallet_timestamp::Config for Runtime {
 	/// A timestamp: milliseconds since the unix epoch.
 	type Moment = u64;
 	type OnTimestampSet = Aura;
-	type MinimumPeriod = ConstU64<{ SLOT_DURATION / 2 }>;
+	type MinimumPeriod = ConstU64<0>;
 	type WeightInfo = weights::pallet_timestamp::WeightInfo<Runtime>;
 }
 
@@ -794,7 +798,7 @@ impl pallet_aura::Config for Runtime {
 	type AuthorityId = AuraId;
 	type DisabledValidators = ();
 	type MaxAuthorities = ConstU32<100_000>;
-	type AllowMultipleBlocksPerSlot = ConstBool<false>;
+	type AllowMultipleBlocksPerSlot = ConstBool<true>;
 	type SlotDuration = ConstU64<SLOT_DURATION>;
 }
 
