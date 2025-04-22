b4d34a8d2d1a90c5e32b563d63c0beca8571b312
Andrei Sandu <andrei-mihail@parity.io>
2025-04-22 13:16:42 +0300

    move import
    Signed-off-by: Andrei Sandu <andrei-mihail@parity.io>

--- relay/kusama/src/lib.rs
+++ relay/kusama/src/lib.rs
@@ -38,7 +38,6 @@ use frame_support::{
 use kusama_runtime_constants::{proxy::ProxyType, system_parachain::coretime::TIMESLICE_PERIOD};
 use pallet_nis::WithMaximumOf;
 use polkadot_primitives::{
-	node_features::FeatureIndex,
 	slashing,
 	vstaging::{CandidateEvent, CommittedCandidateReceiptV2, CoreState, ScrapedOnChainVotes},
 	AccountId, AccountIndex, ApprovalVotingParams, Balance, BlockNumber, CandidateHash, CoreIndex,
@@ -1923,7 +1922,7 @@ pub mod migrations {
 	use super::*;
 	use pallet_balances::WeightInfo;
 	use runtime_parachains::configuration::WeightInfo as ParachainsWeightInfo;
-
+	use polkadot_primitives::node_features::FeatureIndex;
 	/// Enable RFC103 feature.
 	///
 	/// This will make the Kusama relay chain runtime accept v2 candidate receipts.
