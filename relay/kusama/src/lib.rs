6cd80f22b67328c96ae6c751423a61e01659e428
Andrei Sandu <andrei-mihail@parity.io>
2025-04-22 19:15:24 +0300

    spec ++
    Signed-off-by: Andrei Sandu <andrei-mihail@parity.io>

--- relay/kusama/src/lib.rs
+++ relay/kusama/src/lib.rs
@@ -181,7 +181,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: alloc::borrow::Cow::Borrowed("kusama"),
 	impl_name: alloc::borrow::Cow::Borrowed("parity-kusama"),
 	authoring_version: 2,
-	spec_version: 1_004_003,
+	spec_version: 1_005_000,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 26,
