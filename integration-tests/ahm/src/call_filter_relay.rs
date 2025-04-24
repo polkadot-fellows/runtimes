47b4cb5cfe67e610f20ba793aebfecfebb846065
Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
2025-04-24 12:32:41 +0200

    [AHM] Add Asset Hub CallFilter (#670)
    Add Call filtering to disable calls on AH during the migration.
    
    ---------
    
    Signed-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>

--- integration-tests/ahm/src/call_filter_relay.rs
+++ integration-tests/ahm/src/call_filter_relay.rs
@@ -16,65 +16,41 @@
 
 //! Asset Hub Migration tests.
 
-mod accounts;
+use crate::porting_prelude::*;
 
 use frame_support::{sp_runtime::traits::Dispatchable, traits::Contains};
 use pallet_rc_migrator::*;
 use polkadot_primitives::Id as ParaId;
-use polkadot_runtime::{Block, BuildStorage, RcMigrator, Runtime as T, RuntimeOrigin, System};
+use polkadot_runtime::{
+	Block, BuildStorage, RcMigrator, Runtime as T, RuntimeCall, RuntimeOrigin, System,
+};
 use remote_externalities::{Builder, Mode, OfflineConfig, RemoteExternalities};
 use runtime_parachains::inclusion::AggregateMessageOrigin;
 use sp_runtime::AccountId32;
 
-/// Create externalities that have their state initialized from a snapshot.
-///
-/// The path to the snapshot must be provided through the environment variable `SNAP`. If if is not
-/// set, this function will return `None`.
-///
-/// You can create such a snapshot with the [`try-runtime-cli`](https://github.com/paritytech/try-runtime-cli). For example:
-/// `try-runtime create-snapshot --uri wss://rpc.polkadot.io:443 polkadot.snap`.
-async fn remote_ext_test_setup() -> Option<RemoteExternalities<Block>> {
-	sp_tracing::try_init_simple();
-	let snap = std::env::var("SNAP").ok()?;
-	let abs = std::path::absolute(snap.clone());
-
-	let ext = Builder::<Block>::default()
-		.mode(Mode::Offline(OfflineConfig { state_snapshot: snap.clone().into() }))
-		.build()
-		.await
-		.map_err(|e| {
-			eprintln!("Could not load from snapshot: {:?}: {:?}", abs, e);
-		})
-		.unwrap();
-
-	Some(ext)
-}
-
 /// Check that the call filtering mechanism works.
 #[test]
+#[cfg(not(feature = "ahm-westend"))] // FIXME make work on Westend
 fn call_filter_works() {
 	let mut t: sp_io::TestExternalities =
 		frame_system::GenesisConfig::<T>::default().build_storage().unwrap().into();
 
 	// MQ calls are never filtered:
-	let mq_call =
-		polkadot_runtime::RuntimeCall::MessageQueue(pallet_message_queue::Call::<T>::reap_page {
-			message_origin: AggregateMessageOrigin::Ump(
-				runtime_parachains::inclusion::UmpQueueId::Para(ParaId::from(1000)),
-			),
-			page_index: 0,
-		});
+	let mq_call = RuntimeCall::MessageQueue(pallet_message_queue::Call::<T>::reap_page {
+		message_origin: AggregateMessageOrigin::Ump(
+			runtime_parachains::inclusion::UmpQueueId::Para(ParaId::from(1000)),
+		),
+		page_index: 0,
+	});
 	// Balances calls are filtered during the migration:
-	let balances_call =
-		polkadot_runtime::RuntimeCall::Balances(pallet_balances::Call::<T>::transfer_all {
-			dest: AccountId32::from([0; 32]).into(),
-			keep_alive: false,
-		});
+	let balances_call = RuntimeCall::Balances(pallet_balances::Call::<T>::transfer_all {
+		dest: AccountId32::from([0; 32]).into(),
+		keep_alive: false,
+	});
 	// Indices calls are filtered during and after the migration:
-	let indices_call =
-		polkadot_runtime::RuntimeCall::Indices(pallet_indices::Call::<T>::claim { index: 0 });
+	let indices_call = RuntimeCall::Indices(pallet_indices::Call::<T>::claim { index: 0 });
 
-	let is_allowed = |call: &polkadot_runtime::RuntimeCall| Pallet::<T>::contains(call);
+	let is_allowed = |call: &RuntimeCall| Pallet::<T>::contains(call);
 
 	// Try the BaseCallFilter
 	t.execute_with(|| {
@@ -108,10 +84,11 @@ fn call_filter_works() {
 
 	// Try to actually dispatch the calls
 	t.execute_with(|| {
-		<pallet_balances::Pallet<T> as frame_support::traits::Currency<_>>::deposit_creating(
-			&AccountId32::from([0; 32]),
-			u64::MAX.into(),
-		);
+		let _ =
+			<pallet_balances::Pallet<T> as frame_support::traits::Currency<_>>::deposit_creating(
+				&AccountId32::from([0; 32]),
+				u64::MAX.into(),
+			);
 
 		// Before the migration starts
 		{
@@ -143,7 +120,7 @@ fn call_filter_works() {
 }
 
 /// Whether a call is forbidden by the call filter.
-fn is_forbidden(call: &polkadot_runtime::RuntimeCall) -> bool {
+fn is_forbidden(call: &RuntimeCall) -> bool {
 	let Err(err) = call.clone().dispatch(RuntimeOrigin::signed(AccountId32::from([0; 32]))) else {
 		return false;
 	};
--- integration-tests/ahm/src/tests.rs
+++ integration-tests/ahm/src/tests.rs
@@ -33,7 +33,7 @@
 
 use crate::porting_prelude::*;
 
-use super::{mock::*, proxy_test::ProxiesStillWork};
+use super::{checks::SanityChecks, mock::*, proxy_test::ProxiesStillWork};
 use asset_hub_polkadot_runtime::Runtime as AssetHub;
 use cumulus_pallet_parachain_system::PendingUpwardMessages;
 use cumulus_primitives_core::{BlockT, Junction, Location, ParaId};
@@ -58,6 +58,7 @@ use xcm::latest::*;
 use xcm_emulator::{assert_ok, ConvertLocation, WeightMeter};
 
 type RcChecks = (
+	SanityChecks,
 	pallet_rc_migrator::accounts::AccountsMigrator<Polkadot>,
 	pallet_rc_migrator::preimage::PreimageChunkMigrator<Polkadot>,
 	pallet_rc_migrator::preimage::PreimageRequestStatusMigrator<Polkadot>,
@@ -87,6 +88,7 @@ pub type RcPolkadotChecks = (
 pub type RcPolkadotChecks = ();
 
 type AhChecks = (
+	SanityChecks,
 	pallet_rc_migrator::accounts::AccountsMigrator<AssetHub>,
 	pallet_rc_migrator::preimage::PreimageChunkMigrator<AssetHub>,
 	pallet_rc_migrator::preimage::PreimageRequestStatusMigrator<AssetHub>,
@@ -118,7 +120,7 @@ pub type AhPolkadotChecks = ();
 
 #[tokio::test(flavor = "multi_thread", worker_threads = 2)]
 async fn pallet_migration_works() {
-	let Some((mut rc, mut ah)) = load_externalities().await else { return };
+	let (mut rc, mut ah) = load_externalities().await.unwrap();
 
 	// Set the initial migration stage from env var if set.
 	set_initial_migration_stage(&mut rc);
@@ -147,6 +149,13 @@ async fn pallet_migration_works() {
 
 	// Migrate the Asset Hub
 	ah_migrate(&mut ah, dmp_messages);
+
+	ah.execute_with(|| {
+		assert!(
+			pallet_ah_migrator::AhMigrationStage::<AssetHub>::get() ==
+				pallet_ah_migrator::MigrationStage::MigrationDone
+		);
+	});
 
 	// Post-checks on the Asset Hub
 	run_check(|| AhChecks::post_check(rc_pre.unwrap(), ah_pre.unwrap()), &mut ah);
@@ -345,6 +354,7 @@ fn ah_account_migration_weight() {
 	}
 }
 
+#[ignore] // Slow
 #[tokio::test(flavor = "current_thread")]
 async fn migration_works() {
 	let Some((mut rc, mut ah)) = load_externalities().await else { return };
