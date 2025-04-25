249369a5db3eeaa6ca59cd7dc868f711d0134c55
Eugenio Paluello <eugenio@parity.io>
2025-04-25 18:09:23 +0200

    fix: min_delay westend conversion

--- pallets/rc-migrator/src/staking/nom_pools.rs
+++ pallets/rc-migrator/src/staking/nom_pools.rs
@@ -468,8 +468,13 @@ impl<T: Config> crate::types::RcMigrationCheck for NomPoolsMigrator<T> {
 		// Collect bonded pools
 		for (pool_id, mut pool) in pallet_nomination_pools::BondedPools::<T>::iter() {
 			if let Some(ref mut change_rate) = pool.commission.change_rate.as_mut() {
+				#[cfg(not(feature = "ahm-westend"))]
+				{
 				change_rate.min_delay =
-					(change_rate.min_delay / 2u32.into()).saturating_add(tests::One::one());
+					change_rate.min_delay / 2u32.into();
+				}
+				change_rate.min_delay = change_rate.min_delay.saturating_add(tests::One::one());
+				
 			}
 			let generic_pool = tests::GenericBondedPoolInner {
 				commission: tests::GenericCommission {
