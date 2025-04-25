7c139d2ad3dcbb518e98ea4c0bd975a1f2248195
Branislav Kontur <bkontur@gmail.com>
2025-04-25 10:31:31 +0200

    multisig - manual patch needed

--- relay/kusama/src/weights/pallet_multisig.rs
+++ relay/kusama/src/weights/pallet_multisig.rs
@@ -118,7 +118,7 @@ impl<T: frame_system::Config> pallet_multisig::WeightInfo for WeightInfo<T> {
 	/// Proof: `Multisig::Multisigs` (`max_values`: None, `max_size`: Some(3346), added: 5821, mode: `MaxEncodedLen`)
 	/// The range of component `s` is `[2, 100]`.
 	/// The range of component `z` is `[0, 10000]`.
-	fn approve_as_multi_create(s: u32, _z: u32, ) -> Weight {
+	fn approve_as_multi_create(s: u32, /* TODO:needs-patch-for-stable2412 _z: u32,*/ ) -> Weight {
 		// Proof Size summary in bytes:
 		//  Measured:  `301 + s * (2 ±0)`
 		//  Estimated: `6811`
@@ -134,7 +134,7 @@ impl<T: frame_system::Config> pallet_multisig::WeightInfo for WeightInfo<T> {
 	/// Proof: `Multisig::Multisigs` (`max_values`: None, `max_size`: Some(3346), added: 5821, mode: `MaxEncodedLen`)
 	/// The range of component `s` is `[2, 100]`.
 	/// The range of component `z` is `[0, 10000]`.
-	fn approve_as_multi_approve(s: u32, z: u32, ) -> Weight {
+	fn approve_as_multi_approve(s: u32, /* TODO:needs-patch-for-stable2412 z: u32,*/ ) -> Weight {
 		// Proof Size summary in bytes:
 		//  Measured:  `320`
 		//  Estimated: `6811`
@@ -144,7 +144,7 @@ impl<T: frame_system::Config> pallet_multisig::WeightInfo for WeightInfo<T> {
 			// Standard Error: 185
 			.saturating_add(Weight::from_parts(96_015, 0).saturating_mul(s.into()))
 			// Standard Error: 1
-			.saturating_add(Weight::from_parts(8, 0).saturating_mul(z.into()))
+			/* TODO:needs-patch-for-stable2412 .saturating_add(Weight::from_parts(8, 0).saturating_mul(z.into())) */
 			.saturating_add(T::DbWeight::get().reads(1))
 			.saturating_add(T::DbWeight::get().writes(1))
 	}
@@ -152,7 +152,7 @@ impl<T: frame_system::Config> pallet_multisig::WeightInfo for WeightInfo<T> {
 	/// Proof: `Multisig::Multisigs` (`max_values`: None, `max_size`: Some(3346), added: 5821, mode: `MaxEncodedLen`)
 	/// The range of component `s` is `[2, 100]`.
 	/// The range of component `z` is `[0, 10000]`.
-	fn cancel_as_multi(s: u32, _z: u32, ) -> Weight {
+	fn cancel_as_multi(s: u32, /* TODO:needs-patch-for-stable2412 _z: u32,*/ ) -> Weight {
 		// Proof Size summary in bytes:
 		//  Measured:  `492 + s * (1 ±0)`
 		//  Estimated: `6811`
