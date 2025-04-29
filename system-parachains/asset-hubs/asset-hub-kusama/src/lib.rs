f5b3d53f25cf7a78479874badf3aa44c6f7ec49b
muharem <ismailov.m.h@gmail.com>
2025-04-29 11:07:31 +0200

    Enable Async Backing for Kusama Asset Hub (#659)
    Enable Async Backing for Kusama Asset Hub.
    
    Main configuration changes:
    - `UNINCLUDED_SEGMENT_CAPACITY` from 1 to 3
    - `MILLISECS_PER_BLOCK` from 12_000 to 6000
    - `MAXIMUM_BLOCK_WEIGHT` from 0.5 sec to 2 sec
    - `NORMAL_DISPATCH_RATIO` from 75% to 85%, but still 2x (0.125 -> 0.3
    sec) increase for operational transactions
    
    NOTE: `SLOT_DURATION` stays at 12 sec.
    
    The Kusama People Chain, which is already running with async backing,
    has also migrated to use the constant parameters shared with the Kusama
    Asset Hub.
    
    ### Motivation
    This PR enables async backing specifically for the Asset Hub to support
    the upcoming Asset Hub migration. Only Asset Hub is included, as this
    work is part of the ongoing migration effort.
    
    ### Impact:
    - block time is 6 seconds (previously 12 seconds);
    - `pallet_collator_selection`: collators that authored blocks before the
    migration to async backing, may be removed for being stale more quickly
    since `KickThreshold` used timings based on 12 sec block time;
    - `pallet_proxy`: announcements for the proxies with specified `delay`,
    may become active earlier than expected. Currently, there are no proxies
    with a specified `delay` exists;
    - `pallet_nfts`: collections with defined `mint.start_block` and
    `mint.end_block` may start and end their minting period earlier. At
    present, there are no NFT collections with `start_block` or `end_block`
    set.
    
    Once SDK 2503 is integrated into the runtimes, the last two pallets can
    be configured to use the more time-accurate Relay Chain block provider.
    
    ---------
    
    Co-authored-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
    Co-authored-by: Bastian Köcher <git@kchr.de>

--- system-parachains/asset-hubs/asset-hub-kusama/src/lib.rs
+++ system-parachains/asset-hubs/asset-hub-kusama/src/lib.rs
@@ -83,10 +83,19 @@ use parachains_common::{
 	Header, Nonce, Signature,
 };
 use sp_runtime::RuntimeDebug;
-pub use system_parachains_constants::SLOT_DURATION;
+pub use system_parachains_constants::async_backing::SLOT_DURATION;
 use system_parachains_constants::{
-	kusama::{consensus::*, currency::*, fee::WeightToFee},
-	AVERAGE_ON_INITIALIZE_RATIO, DAYS, HOURS, MAXIMUM_BLOCK_WEIGHT, NORMAL_DISPATCH_RATIO,
+	async_backing::{
+		AVERAGE_ON_INITIALIZE_RATIO, DAYS, HOURS, MAXIMUM_BLOCK_WEIGHT, NORMAL_DISPATCH_RATIO,
+	},
+	kusama::{
+		consensus::{
+			async_backing::UNINCLUDED_SEGMENT_CAPACITY, BLOCK_PROCESSING_VELOCITY,
+			RELAY_CHAIN_SLOT_DURATION_MILLIS,
+		},
+		currency::*,
+		fee::WeightToFee,
+	},
 };
 use xcm::{
 	latest::prelude::{AssetId, BodyId},
@@ -208,7 +217,7 @@ impl pallet_timestamp::Config for Runtime {
 	/// A timestamp: milliseconds since the unix epoch.
 	type Moment = u64;
 	type OnTimestampSet = Aura;
-	type MinimumPeriod = ConstU64<{ SLOT_DURATION / 2 }>;
+	type MinimumPeriod = ConstU64<0>;
 	type WeightInfo = weights::pallet_timestamp::WeightInfo<Runtime>;
 }
 
@@ -800,7 +809,7 @@ impl pallet_aura::Config for Runtime {
 	type AuthorityId = AuraId;
 	type DisabledValidators = ();
 	type MaxAuthorities = ConstU32<100_000>;
-	type AllowMultipleBlocksPerSlot = ConstBool<false>;
+	type AllowMultipleBlocksPerSlot = ConstBool<true>;
 	type SlotDuration = ConstU64<SLOT_DURATION>;
 }
 
