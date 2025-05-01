eb13483b768ea73f17ab8a8cafeacdc2921fcd14
Dom <polkadotdom@gmail.com>
2025-05-01 16:43:14 -0400

    Add scheduler to westend tests

--- integration-tests/ahm/src/tests.rs
+++ integration-tests/ahm/src/tests.rs
@@ -76,6 +76,7 @@ type RcChecks = (
 	pallet_rc_migrator::staking::fast_unstake::FastUnstakeMigrator<Polkadot>,
 	pallet_rc_migrator::conviction_voting::ConvictionVotingMigrator<Polkadot>,
 	pallet_rc_migrator::asset_rate::AssetRateMigrator<Polkadot>,
+	pallet_rc_migrator::scheduler::SchedulerMigrator<Polkadot>,
 	RcPolkadotChecks,
 	// other checks go here (if available on Polkadot, Kusama and Westend)
 	ProxiesStillWork,
@@ -89,7 +90,6 @@ pub type RcPolkadotChecks = (
 	pallet_rc_migrator::referenda::ReferendaMigrator<Polkadot>,
 	pallet_rc_migrator::claims::ClaimsMigrator<Polkadot>,
 	pallet_rc_migrator::crowdloan::CrowdloanMigrator<Polkadot>,
-	pallet_rc_migrator::scheduler::SchedulerMigrator<Polkadot>,
 );
 
 #[cfg(not(feature = "ahm-polkadot"))]
@@ -108,6 +108,7 @@ type AhChecks = (
 	pallet_rc_migrator::staking::fast_unstake::FastUnstakeMigrator<AssetHub>,
 	pallet_rc_migrator::conviction_voting::ConvictionVotingMigrator<AssetHub>,
 	pallet_rc_migrator::asset_rate::AssetRateMigrator<AssetHub>,
+	pallet_rc_migrator::scheduler::SchedulerMigrator<AssetHub>,
 	AhPolkadotChecks,
 	// other checks go here (if available on Polkadot, Kusama and Westend)
 	ProxiesStillWork,
@@ -122,7 +123,6 @@ pub type AhPolkadotChecks = (
 	pallet_rc_migrator::referenda::ReferendaMigrator<AssetHub>,
 	pallet_rc_migrator::claims::ClaimsMigrator<AssetHub>,
 	pallet_rc_migrator::crowdloan::CrowdloanMigrator<AssetHub>,
-	pallet_rc_migrator::scheduler::SchedulerMigrator<AssetHub>,
 );
 
 #[cfg(not(feature = "ahm-polkadot"))]
