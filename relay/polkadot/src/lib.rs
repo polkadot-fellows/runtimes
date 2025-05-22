2abdf3c32f78f4b88b4c02828280583214625a6e
Francisco Aguirre <franciscoaguirreperez@gmail.com>
2025-05-22 10:43:58 +0200

    feat: bump crates and relay versions

--- relay/polkadot/src/lib.rs
+++ relay/polkadot/src/lib.rs
@@ -168,7 +168,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: alloc::borrow::Cow::Borrowed("polkadot"),
 	impl_name: alloc::borrow::Cow::Borrowed("parity-polkadot"),
 	authoring_version: 0,
-	spec_version: 1_005_000,
+	spec_version: 1_005_001,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 26,
