65f568c964f1447b6d9b789007f21c66d60a998f
Alexandru Vasile <alexandru.vasile@parity.io>
2025-06-09 12:49:02 +0000

    asset-hub-dot/config: Set minimum period to Zero
    Signed-off-by: Alexandru Vasile <alexandru.vasile@parity.io>

--- system-parachains/asset-hubs/asset-hub-polkadot/src/lib.rs
+++ system-parachains/asset-hubs/asset-hub-polkadot/src/lib.rs
@@ -249,7 +249,7 @@ impl pallet_timestamp::Config for Runtime {
 	/// A timestamp: milliseconds since the unix epoch.
 	type Moment = u64;
 	type OnTimestampSet = Aura;
-	type MinimumPeriod = ConstU64<{ SLOT_DURATION / 2 }>;
+	type MinimumPeriod = ConstU64<0>;
 	type WeightInfo = weights::pallet_timestamp::WeightInfo<Runtime>;
 }
 
