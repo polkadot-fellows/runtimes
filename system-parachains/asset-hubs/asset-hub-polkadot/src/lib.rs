34ed17d05bf1bb6f5f81a57e8f40f2348f008260
Alexandru Vasile <alexandru.vasile@parity.io>
2025-06-09 12:49:40 +0000

    asset-hub-dot/aura: Allow multiple blocks per slot for async backing
    Signed-off-by: Alexandru Vasile <alexandru.vasile@parity.io>

--- system-parachains/asset-hubs/asset-hub-polkadot/src/lib.rs
+++ system-parachains/asset-hubs/asset-hub-polkadot/src/lib.rs
@@ -757,7 +757,7 @@ impl pallet_aura::Config for Runtime {
 	type AuthorityId = AuraId;
 	type DisabledValidators = ();
 	type MaxAuthorities = ConstU32<100_000>;
-	type AllowMultipleBlocksPerSlot = ConstBool<false>;
+	type AllowMultipleBlocksPerSlot = ConstBool<true>;
 	type SlotDuration = ConstU64<SLOT_DURATION>;
 }
 
