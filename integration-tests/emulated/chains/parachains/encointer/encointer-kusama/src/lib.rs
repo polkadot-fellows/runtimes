b822b83ea0899f9f6dfa3b37a7f711e661e833c2
Christian Langenbacher <clangenb+gh@protonmail.ch>
2025-06-24 17:59:42 +0200

    fix teleport integration tests except delivery fee

--- integration-tests/emulated/chains/parachains/encointer/encointer-kusama/src/lib.rs
+++ integration-tests/emulated/chains/parachains/encointer/encointer-kusama/src/lib.rs
@@ -13,7 +13,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-mod genesis;
+pub mod genesis;
 
 // Substrate
 use frame_support::traits::OnInitialize;
