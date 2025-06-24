281fc1e7092e1eb1d795cbc8802a977a3c6431ea
Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
2025-06-24 17:52:00 +0200

    Bump specs to 1.6.1
    Signed-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>

--- relay/kusama/src/lib.rs
+++ relay/kusama/src/lib.rs
@@ -185,7 +185,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: alloc::borrow::Cow::Borrowed("kusama"),
 	impl_name: alloc::borrow::Cow::Borrowed("parity-kusama"),
 	authoring_version: 2,
-	spec_version: 1_006_000,
+	spec_version: 1_006_001,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 26,
