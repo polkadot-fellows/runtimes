7aec19ac402017ff19640eab3336eb8044931f99
Clara van Staden <claravanstaden64@gmail.com>
2025-06-26 10:38:53 +0200

    Update integration-tests/emulated/tests/bridges/bridge-hub-polkadot/src/tests/snowbridge.rs
    Co-authored-by: Adrian Catangiu <adrian@parity.io>

--- integration-tests/emulated/tests/bridges/bridge-hub-polkadot/src/tests/snowbridge.rs
+++ integration-tests/emulated/tests/bridges/bridge-hub-polkadot/src/tests/snowbridge.rs
@@ -150,7 +150,6 @@ fn send_token_from_ethereum_to_penpal() {
 	// The Weth asset location, identified by the contract address on Ethereum
 	let weth_asset_location: Location =
 		(Parent, Parent, EthereumNetwork::get(), AccountKey20 { network: None, key: WETH }).into();
-	let weth_asset_location_latest: Location = weth_asset_location.clone();
 	// Converts the Weth asset location into an asset ID
 
 	// Fund ethereum sovereign on AssetHub
