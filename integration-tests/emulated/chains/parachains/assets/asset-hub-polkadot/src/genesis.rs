a833b537b109fd4f15abcfc336a4e9155d2a5f4d
Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
2025-07-03 13:41:24 +0200

    fmt
    Signed-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>

--- integration-tests/emulated/chains/parachains/assets/asset-hub-polkadot/src/genesis.rs
+++ integration-tests/emulated/chains/parachains/assets/asset-hub-polkadot/src/genesis.rs
@@ -19,8 +19,8 @@ use sp_keyring::{Ed25519Keyring, Sr25519Keyring};
 // Cumulus
 use asset_hub_polkadot_runtime::xcm_config::bridging::to_ethereum::EthereumNetwork;
 use emulated_integration_tests_common::{
-	accounts, build_genesis_storage, xcm_emulator::ConvertLocation, RESERVABLE_ASSET_ID,
-	SAFE_XCM_VERSION,
+	accounts, build_genesis_storage, xcm_emulator::ConvertLocation, ASSETS_PALLET_ID,
+	RESERVABLE_ASSET_ID, SAFE_XCM_VERSION, TELEPORTABLE_ASSET_ID,
 };
 use frame_support::sp_runtime::traits::AccountIdConversion;
 use integration_tests_helpers::common::snowbridge::{
@@ -30,7 +30,6 @@ use parachains_common::{AccountId, Balance};
 use polkadot_parachain_primitives::primitives::Sibling;
 use snowbridge_inbound_queue_primitives::EthereumLocationsConverterFor;
 use xcm::prelude::*;
-use emulated_integration_tests_common::{ASSETS_PALLET_ID, TELEPORTABLE_ASSET_ID};
 
 pub const PARA_ID: u32 = 1000;
 pub const ED: Balance = asset_hub_polkadot_runtime::ExistentialDeposit::get();
