d13d14e68c34ab12e55ee7f65da282bd79d15b5f
Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
2025-07-09 18:36:16 +0200

    more fix
    Signed-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>

--- system-parachains/asset-hubs/asset-hub-polkadot/src/staking/mod.rs
+++ system-parachains/asset-hubs/asset-hub-polkadot/src/staking/mod.rs
@@ -144,6 +144,30 @@ impl pallet_delegated_staking::Config for Runtime {
 	type CoreStaking = Staking;
 }
 
+#[cfg(feature = "runtime-benchmarks")]
+parameter_types! {
+	pub BenchElectionBounds: frame_election_provider_support::bounds::ElectionBounds =
+		frame_election_provider_support::bounds::ElectionBoundsBuilder::default().build();
+}
+
+#[cfg(feature = "runtime-benchmarks")]
+pub struct OnChainConfig;
+
+#[cfg(feature = "runtime-benchmarks")]
+impl frame_election_provider_support::onchain::Config for OnChainConfig {
+	// unbounded
+	type Bounds = BenchElectionBounds;
+	// We should not need sorting, as our bounds are large enough for the number of
+	// nominators/validators in this test setup.
+	type Sort = ConstBool<false>;
+	type DataProvider = Staking;
+	type MaxBackersPerWinner = MaxBackersPerWinner;
+	type MaxWinnersPerPage = MaxWinnersPerPage;
+	type Solver = frame_election_provider_support::SequentialPhragmen<AccountId, Perbill>;
+	type System = Runtime;
+	type WeightInfo = ();
+}
+
 impl multi_block::Config for Runtime {
 	type RuntimeEvent = RuntimeEvent;
 	type Pages = Pages;
