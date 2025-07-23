0bc20f235104f55a3333f7bdb8b5e02908809429
Ankan <ankan.anurag@gmail.com>
2025-07-23 21:19:10 +0200

    identify with only validator account

--- relay/polkadot/src/lib.rs
+++ relay/polkadot/src/lib.rs
@@ -507,7 +507,7 @@ impl pallet_session::Config for Runtime {
 impl pallet_session::historical::Config for Runtime {
 	type RuntimeEvent = RuntimeEvent;
 	type FullIdentification = pallet_staking::Exposure<AccountId, Balance>;
-	type FullIdentificationOf = pallet_staking::ExposureOf<Runtime>;
+	type FullIdentificationOf = pallet_staking::DefaultExposureOf<Runtime>;
 }
 
 parameter_types! {
