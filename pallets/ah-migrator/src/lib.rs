ea5442f073ca64e207329954c5f51bf15786f333
Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
2025-07-28 18:17:19 +0200

    use weight function
    Signed-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>

--- pallets/ah-migrator/src/lib.rs
+++ pallets/ah-migrator/src/lib.rs
@@ -863,10 +863,7 @@ pub mod pallet {
 		}
 
 		#[pallet::call_index(24)]
-		#[pallet::weight(
-			// TODO @ggwpez weight
-			T::DbWeight::get().reads_writes(3, 3).saturating_add(Weight::from_parts(10_000_000, 200)).saturating_mul(messages.len() as u64)
-		)]
+		#[pallet::weight(T::AhWeightInfo::receive_child_bounties_messages(messages.len() as u32))]
 		pub fn receive_child_bounties_messages(
 			origin: OriginFor<T>,
 			messages: Vec<PortableChildBountiesMessage>,
