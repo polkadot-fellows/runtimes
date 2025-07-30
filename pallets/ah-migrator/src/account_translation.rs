6c1c2694019002fadcaf226daa55a7b5feaa4ea6
Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
2025-07-30 20:47:46 +0200

    doc comment
    Signed-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>

--- pallets/ah-migrator/src/account_translation.rs
+++ pallets/ah-migrator/src/account_translation.rs
@@ -39,6 +39,7 @@ impl<T: Config> Pallet<T> {
 		new
 	}
 
+	/// Translate the account if its a parachain sovereign account.
 	fn maybe_sovereign_translate(account: &T::AccountId) -> Option<T::AccountId> {
 		let Some(new) = crate::sovereign_account_translation::SOV_TRANSLATIONS
 			.binary_search_by_key(account, |(rc_acc, _)| rc_acc.clone())
@@ -63,6 +64,7 @@ impl<T: Config> Pallet<T> {
 		Some(new)
 	}
 
+	/// Translate the account if its derived from a parachain sovereign account.
 	fn maybe_derived_translate(account: &T::AccountId) -> Option<T::AccountId> {
 		let Some((new, idx)) = crate::sovereign_account_translation::DERIVED_TRANSLATIONS
 			.binary_search_by_key(account, |(rc_acc, _, _)| rc_acc.clone())
