b18b543a9a90551e00db0ed50f7e19b7e361aeed
muharem <ismailov.m.h@gmail.com>
2025-08-06 16:40:54 +0200

    [AHM] Unprocessed msg buffer size 50 (#839)
    Unprocessed msg buffer size 50
    
    After reviewing the logs from the Westend migration run, we observed
    that confirmation messages from AssetHub (AH) to the Relay Chain (RC)
    are not received immediately - in some cases, the delay is up to 6 RC
    blocks. This significantly slows down the migration process when the
    buffer size is small.
    
    Currently, due to weight limits, we send at most 4 messages per block
    during account migration. Each message can be up to 64 KiB. A collator
    can include up to 1/6 of the PoV size (1.6 MB) per block. Additionally,
    we meter AH’s weight on the RC side during data extraction, ensuring we
    don’t send more per block than AH can handle.
    
    Given these constraints, increasing the buffer size to 50 appears
    reasonable and should notably improve migration throughput.

--- relay/polkadot/src/lib.rs
+++ relay/polkadot/src/lib.rs
@@ -1737,7 +1737,7 @@ impl pallet_rc_migrator::Config for Runtime {
 	type RcPostMigrationCalls = ahm_phase1::CallsEnabledAfterMigration;
 	type StakingDelegationReason = ahm_phase1::StakingDelegationReason;
 	type OnDemandPalletId = OnDemandPalletId;
-	type UnprocessedMsgBuffer = ConstU32<8>;
+	type UnprocessedMsgBuffer = ConstU32<50>;
 	type XcmResponseTimeout = XcmResponseTimeout;
 	type MessageQueue = MessageQueue;
 	type AhUmpQueuePriorityPattern = AhUmpQueuePriorityPattern;
