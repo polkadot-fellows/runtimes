c22c7e723e2618658b15eb296f359f1e37e3815a
ron <yrong1997@gmail.com>
2025-08-14 03:35:02 +0000

    More comments

--- system-parachains/asset-hubs/asset-hub-polkadot/src/weights/snowbridge_pallet_system_backend.rs
+++ system-parachains/asset-hubs/asset-hub-polkadot/src/weights/snowbridge_pallet_system_backend.rs
@@ -22,22 +22,23 @@ use frame_support::{traits::Get, weights::Weight};
 use core::marker::PhantomData;
 
 /// Weight functions for `snowbridge_pallet_system_frontend::BackendWeightInfo`.
-/// NOTE: Copy the weight generated for `fn register_token() -> Weight` from
-/// ../../../../bridge-hubs/bridge-hub-westend/src/weights/snowbridge_pallet_system_v2.rs
 pub struct WeightInfo<T>(PhantomData<T>);
 impl<T: frame_system::Config> snowbridge_pallet_system_frontend::BackendWeightInfo for WeightInfo<T> {
+	/// Copy the weight generated for `fn register_token() -> Weight` from ../../../../bridge-hubs/bridge-hub-polkadot/src/weights/snowbridge_pallet_system_v2.rs
 	fn transact_register_token() -> Weight {
 		Weight::from_parts(45_000_000, 6044)
 			.saturating_add(T::DbWeight::get().reads(5_u64))
 			.saturating_add(T::DbWeight::get().writes(3_u64))
 	}
 
+	/// Copy the weight generated for `fn add_tip() -> Weight` from ../../../../bridge-hubs/bridge-hub-polkadot/src/weights/snowbridge_pallet_system_v2.rs
 	fn transact_add_tip() -> Weight {
 		Weight::from_parts(45_000_000, 6044)
 			.saturating_add(T::DbWeight::get().reads(5_u64))
 			.saturating_add(T::DbWeight::get().writes(3_u64))
 	}
 
+	/// Copy the weight generated for `fn do_process_message() -> Weight` from ../../../../bridge-hubs/bridge-hub-polkadot/src/weights/snowbridge_pallet_outbound_queue_v2.rs
 	fn do_process_message() -> Weight {
 		Weight::from_parts(19_000_000, 0)
 			.saturating_add(Weight::from_parts(0, 1527))
@@ -45,12 +46,14 @@ impl<T: frame_system::Config> snowbridge_pallet_system_frontend::BackendWeightIn
 			.saturating_add(T::DbWeight::get().writes(4))
 	}
 
+	/// Copy the weight generated for `fn commit_single() -> Weight` from ../../../../bridge-hubs/bridge-hub-polkadot/src/weights/snowbridge_pallet_outbound_queue_v2.rs
 	fn commit_single() -> Weight {
 		Weight::from_parts(10_000_000, 0)
 			.saturating_add(Weight::from_parts(0, 1620))
 			.saturating_add(T::DbWeight::get().reads(1))
 	}
 
+	/// Copy the weight generated for `fn submit_delivery_receipt() -> Weight` from ../../../../bridge-hubs/bridge-hub-polkadot/src/weights/snowbridge_pallet_outbound_queue_v2.rs
 	fn submit_delivery_receipt() -> Weight {
 		Weight::from_parts(68_000_000, 0)
 			.saturating_add(Weight::from_parts(0, 3785))
