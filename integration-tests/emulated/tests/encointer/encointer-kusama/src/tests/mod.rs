67b23c062155a33f01bd82738dd4b926a14eeb1c
Christian Langenbacher <clangenb+gh@protonmail.ch>
2025-08-19 07:35:57 +0200

    add licence headers

--- integration-tests/emulated/tests/encointer/encointer-kusama/src/tests/mod.rs
+++ integration-tests/emulated/tests/encointer/encointer-kusama/src/tests/mod.rs
@@ -1,2 +1,18 @@
+// Copyright (c) 2023 Encointer Association
+// This file is part of Encointer
+//
+// Encointer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Encointer is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Encointer.  If not, see <http://www.gnu.org/licenses/>.
+
 mod remote_treasury_payout;
 mod teleport;
--- integration-tests/emulated/tests/encointer/encointer-kusama/src/tests/remote_treasury_payout.rs
+++ integration-tests/emulated/tests/encointer/encointer-kusama/src/tests/remote_treasury_payout.rs
@@ -1,3 +1,19 @@
+// Copyright (c) 2023 Encointer Association
+// This file is part of Encointer
+//
+// Encointer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Encointer is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Encointer.  If not, see <http://www.gnu.org/licenses/>.
+
 use crate::*;
 use emulated_integration_tests_common::{xcm_emulator::ConvertLocation, USDT_ID};
 use encointer_kusama_runtime::{
