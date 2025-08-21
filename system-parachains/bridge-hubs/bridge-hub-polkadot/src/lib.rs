a69b535df70048ff5941cdd4fe30bb6e753187d5
Adrian Catangiu <adrian@parity.io>
2025-08-21 13:59:45 +0300

    update CHANGELOG and bump spec version

--- system-parachains/bridge-hubs/bridge-hub-polkadot/src/lib.rs
+++ system-parachains/bridge-hubs/bridge-hub-polkadot/src/lib.rs
@@ -194,7 +194,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: Cow::Borrowed("bridge-hub-polkadot"),
 	impl_name: Cow::Borrowed("bridge-hub-polkadot"),
 	authoring_version: 1,
-	spec_version: 1_006_001,
+	spec_version: 1_007_000,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 4,
