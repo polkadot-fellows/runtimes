67dc0c5340700a5fab6a123f79582448afd66bf0
clangenb <37865735+clangenb@users.noreply.github.com>
2025-08-23 10:05:23 +0200

    [Encointer] use XCM V5 to remotely spend funds from encointer treasury accounts on AHK (#679)
    Encointer's treasury beneficiaries would greatly benefit from receiving
    assets directly on asset hub (especially in the future, as we can assume
    that CEX exchanges will allow deposits from asset hub). Additionally, we
    want to give them stability by receiving funds in stable coins
    (potentially dUSD).
    
    The remote transfer has been inspired by the fellowships approach,
    however, it is slightly different as encointer has to pay for that
    execution. Additionally, we need to have a configurable sender account
    on Asset Hub, as each encointer-community has its own unique treasury
    account. Similarly to the polkadot-sdk's pay trait, we have introduced
    the
    [Transfer](https://github.com/encointer/pallets/blob/b3da5c3707569334b5886ea9df4769647d590b6a/treasuries/src/payout.rs#L13)
    trait (mostly in order to allow for a configurable sender account on
    asset hub). Which we intend to upstream after testing and deploying it
    here.
    
    The idea is that the following happens on asset hub:
    1. Descend into the origin of the encointer-treasury account's
    representation on asset hub
    2. Withdraw some native tokens to pay for the fees
    3. Withdraw the token to be transferred from the treasury-account to the
    beneficiary
    4. Transfer: treasury -> beneficiary
    5. Refund the excess weight
    
    ## Changes in this PR:
    - [x] Integrate https://github.com/encointer/pallets/pull/422 and enable
    transferring remotely on asset hub from the encointer runtime
    - [x] Add an encointer-kusama-emulated chain to test
    - [x] Add integration tests
    - [x] Verify that hardcoded remote execution fee of the remote transfer
    is correct
       - [x] Execute Remote transfer on Assethub
    - [x] Add some more XCM integration tests inspired by the other runtimes
    
    ## Todo:
    - [x] After testing, publish encointers crate enabling that feature and
    remove patches
    
    ---------
    
    Co-authored-by: Francisco Aguirre <franciscoaguirreperez@gmail.com>

---
+++ system-parachains/encointer/src/impls.rs
@@ -0,0 +1,103 @@
+// Copyright (c) 2023 Encointer Association
+// This file is part of Encointer
+//
+// Encointer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Encointer is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Encointer.  If not, see <http://www.gnu.org/licenses/>.
+
+//! Some trait implementations for the encointer-kusama-runtime.
+
+#[cfg(feature = "runtime-benchmarks")]
+pub mod benchmarks {
+	use crate::{ParachainSystem, PolkadotXcm, RuntimeOrigin};
+	use core::marker::PhantomData;
+	use cumulus_primitives_core::{ChannelStatus, GetChannelInfo};
+	use frame_support::traits::tokens::PaymentStatus;
+	use pallet_encointer_treasuries::Transfer;
+	use sp_core::Get;
+	use xcm::{latest::Location, opaque::latest::Junction::Parachain, GetVersion};
+
+	/// Trait for setting up any prerequisites for successful execution of benchmarks.
+	pub trait EnsureSuccessful {
+		fn ensure_successful();
+	}
+
+	/// Implementation of the [`EnsureSuccessful`] trait which opens an HRMP channel between
+	/// the Collectives and a parachain with a given ID.
+	pub struct OpenHrmpChannel<I>(PhantomData<I>);
+	impl<I: Get<u32>> EnsureSuccessful for OpenHrmpChannel<I> {
+		fn ensure_successful() {
+			let para_id = I::get();
+
+			// open HRMP channel
+			if let ChannelStatus::Closed = ParachainSystem::get_channel_status(para_id.into()) {
+				log::info!("OpenHrmpChannel: opening channel");
+				ParachainSystem::open_outbound_hrmp_channel_for_benchmarks_or_tests(para_id.into())
+			}
+
+			// set XCM version for sibling parachain
+			let sibling_parachain = Location::new(1, [Parachain(para_id)]);
+			if PolkadotXcm::get_version_for(&sibling_parachain).is_none() {
+				if let Err(e) = PolkadotXcm::force_xcm_version(
+					RuntimeOrigin::root(),
+					sibling_parachain.into(),
+					system_parachains_constants::genesis_presets::SAFE_XCM_VERSION,
+				) {
+					log::error!(
+						"Failed to `force_xcm_version` for para_id: {para_id:?}, error: {e:?}"
+					);
+				}
+			}
+		}
+	}
+
+	/// Type that wraps a type implementing the [`Transfer`] trait to decorate its
+	/// [`Transfer::ensure_successful`] function with a provided implementation of the
+	/// [`EnsureSuccessful`] trait.
+	pub struct TransferWithEnsure<O, E>(PhantomData<(O, E)>);
+	impl<O, E> Transfer for TransferWithEnsure<O, E>
+	where
+		O: Transfer,
+		E: EnsureSuccessful,
+	{
+		type AssetKind = O::AssetKind;
+		type Payer = O::Payer;
+		type Balance = O::Balance;
+		type Beneficiary = O::Beneficiary;
+		type Error = O::Error;
+		type Id = O::Id;
+
+		fn transfer(
+			from: &Self::Payer,
+			to: &Self::Beneficiary,
+			asset_kind: Self::AssetKind,
+			amount: Self::Balance,
+		) -> Result<Self::Id, Self::Error> {
+			O::transfer(from, to, asset_kind, amount)
+		}
+		fn check_payment(id: Self::Id) -> PaymentStatus {
+			O::check_payment(id)
+		}
+		fn ensure_successful(
+			from: &Self::Payer,
+			to: &Self::Beneficiary,
+			asset_kind: Self::AssetKind,
+			amount: Self::Balance,
+		) {
+			E::ensure_successful();
+			O::ensure_successful(from, to, asset_kind, amount)
+		}
+		fn ensure_concluded(id: Self::Id) {
+			O::ensure_concluded(id)
+		}
+	}
+}
