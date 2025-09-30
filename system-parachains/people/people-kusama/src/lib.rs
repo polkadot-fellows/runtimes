74a1c7ea13d1ed1aa87950b1094ab4d6c5715cd8
Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
2025-09-30 12:56:05 +0200

    Bump spec to 1.9.1
    Signed-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>

--- system-parachains/people/people-kusama/src/lib.rs
+++ system-parachains/people/people-kusama/src/lib.rs
@@ -175,7 +175,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: Cow::Borrowed("people-kusama"),
 	impl_name: Cow::Borrowed("people-kusama"),
 	authoring_version: 1,
-	spec_version: 1_009_000,
+	spec_version: 1_009_001,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
