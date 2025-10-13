9a8c1d0bb9fb81026a34308614ff931dff1f6ff6
Branislav Kontur <bkontur@gmail.com>
2025-10-13 09:01:22 +0200

    Fix TODO: https://github.com/polkadot-fellows/runtimes/issues/894

--- system-parachains/asset-hubs/asset-hub-polkadot/src/weights/pallet_election_provider_multi_block_verifier.rs
+++ system-parachains/asset-hubs/asset-hub-polkadot/src/weights/pallet_election_provider_multi_block_verifier.rs
@@ -45,6 +45,8 @@
 
 use frame_support::{traits::Get, weights::Weight};
 use core::marker::PhantomData;
+// TODO: https://github.com/polkadot-fellows/runtimes/issues/894
+use pallet_election_provider_multi_block::weights::traits::pallet_election_provider_multi_block_verifier;
 
 /// Weight functions for `pallet_election_provider_multi_block_verifier`.
 pub struct WeightInfo<T>(PhantomData<T>);
