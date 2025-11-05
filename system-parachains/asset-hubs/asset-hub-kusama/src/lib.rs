026099d5566293f55a2690f9816d44a728f04869
kianenigma <kian@parity.io>
2025-11-05 12:10:25 +0000

    fmt

--- system-parachains/asset-hubs/asset-hub-kusama/src/lib.rs
+++ system-parachains/asset-hubs/asset-hub-kusama/src/lib.rs
@@ -714,7 +714,8 @@ impl InstanceFilter<RuntimeCall> for ProxyType {
 						RuntimeCall::Utility { .. } |
 						RuntimeCall::Multisig { .. } |
 						RuntimeCall::NftFractionalization { .. } |
-						RuntimeCall::Nfts { .. } | RuntimeCall::Uniques { .. }
+						RuntimeCall::Nfts { .. } |
+						RuntimeCall::Uniques { .. }
 				)
 			},
 			ProxyType::AssetOwner => matches!(
