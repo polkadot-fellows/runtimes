4eebfe305f044cf27c9937fc0d15d6c1baff97e3
Dónal Murray <donal.murray@parity.io>
2025-11-06 18:53:48 +0000

    Undo relay changes

--- relay/polkadot/src/governance/mod.rs
+++ relay/polkadot/src/governance/mod.rs
@@ -48,8 +48,8 @@ impl pallet_conviction_voting::Config for Runtime {
 
 parameter_types! {
 	pub const AlarmInterval: BlockNumber = 1;
-	pub const SubmissionDeposit: Balance = 10 * DOLLARS;
-	pub const UndecidingTimeout: BlockNumber = 3 * DAYS;
+	pub const SubmissionDeposit: Balance = DOLLARS;
+	pub const UndecidingTimeout: BlockNumber = 14 * DAYS;
 }
 
 parameter_types! {
