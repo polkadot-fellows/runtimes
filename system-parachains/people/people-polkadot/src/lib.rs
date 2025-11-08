c1a537c4a2b2e55624c491703f5ebbc8c8f33888
Kian Paimani <5588131+kianenigma@users.noreply.github.com>
2025-11-08 13:39:50 +0000

    Release 2.0.2 (#1004)
    Needs enactment on PAH + KAH.
    
    Co-authored-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>

--- system-parachains/people/people-polkadot/src/lib.rs
+++ system-parachains/people/people-polkadot/src/lib.rs
@@ -158,7 +158,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: Cow::Borrowed("people-polkadot"),
 	impl_name: Cow::Borrowed("people-polkadot"),
 	authoring_version: 1,
-	spec_version: 2_000_001,
+	spec_version: 2_000_002,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 0,
