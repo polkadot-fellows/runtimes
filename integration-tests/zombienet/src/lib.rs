55888306d79e7735bfdc693c32372084dc3a4d13
Javier Viola <363911+pepoviola@users.noreply.github.com>
2025-11-14 17:18:54 +0100

    fmt

--- integration-tests/zombienet/src/lib.rs
+++ integration-tests/zombienet/src/lib.rs
@@ -37,16 +37,16 @@ pub fn small_network() -> Result<NetworkConfig, Error> {
 				.with_collator(|n| n.with_name(COLLATOR))
 		});
 
-		let config = if let Ok(local_ip) = std::env::var("ZOMBIE_LOCAL_IP") {
-			config.with_global_settings(|s| s.with_local_ip(&local_ip))
-		} else {
-			config
-		};
+	let config = if let Ok(local_ip) = std::env::var("ZOMBIE_LOCAL_IP") {
+		config.with_global_settings(|s| s.with_local_ip(&local_ip))
+	} else {
+		config
+	};
 
-		let config = config.build().map_err(|errs| {
-			let e = errs.iter().fold("".to_string(), |memo, err| format!("{memo} \n {err}"));
-			anyhow!(e)
-		})?;
+	let config = config.build().map_err(|errs| {
+		let e = errs.iter().fold("".to_string(), |memo, err| format!("{memo} \n {err}"));
+		anyhow!(e)
+	})?;
 
 	Ok(config)
 }
