5c75dec0078fcbf0d0aa20371775533c0d6270f1
claravanstaden <claravanstaden64@gmail.com>
2025-11-20 08:14:36 +0200

    release 2.0.3

--- system-parachains/bridge-hubs/bridge-hub-polkadot/src/lib.rs
+++ system-parachains/bridge-hubs/bridge-hub-polkadot/src/lib.rs
@@ -206,7 +206,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: Cow::Borrowed("bridge-hub-polkadot"),
 	impl_name: Cow::Borrowed("bridge-hub-polkadot"),
 	authoring_version: 1,
-	spec_version: 2_000_002,
+	spec_version: 2_000_003,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 4,
