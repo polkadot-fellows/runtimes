4d6e924e5ffde37f2430b573137b250a3977e028
Tom <tsenseless@gmail.com>
2025-11-20 21:41:43 -0500

    Update install-deps.sh

--- .github/install-deps.sh
+++ .github/install-deps.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 sudo apt update
-sudo apt install --assume-yes openssl pkg-config g++ make cmake protobuf-compiler libssl-dev libclang-dev libudev-dev git lz4
+sudo apt install --assume-yes openssl pkg-config g++ make cmake protobuf-compiler libssl-dev libclang-dev libudev-dev git lz4 yarn
 
 # Free space on the runner
 df -h
