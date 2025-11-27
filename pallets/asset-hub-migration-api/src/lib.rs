1b8dbdd1a7a0fb058c9dc78c73e2c66d57e5a4d0
Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
2025-11-27 22:37:35 +0200

    fix
    Signed-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>

--- pallets/asset-hub-migration-api/src/lib.rs
+++ pallets/asset-hub-migration-api/src/lib.rs
@@ -13,7 +13,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//! Runtime API for the Asset Hub migration process.
+//! Runtime API for fetching info about the Asset Hub migration.
 
 #![cfg_attr(not(feature = "std"), no_std)]
 
