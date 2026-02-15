008aa4067e94fa03d2d86c65ae18018c96c17e67
David Salami <wizdave97@gmail.com>
2026-02-15 07:50:16 +0000

    use btree set to collect parachain ids

--- relay/polkadot/src/lib.rs
+++ relay/polkadot/src/lib.rs
@@ -428,8 +428,10 @@ impl BeefyDataProvider<H256> for ParaHeadsRootProvider {
 	fn extra_data() -> H256 {
 		// Manually add hyperbridge to parachains header root
 		let hyperbridge: u32 = 3367;
-		let mut parachains = parachains_paras::Parachains::<Runtime>::get();
-		parachains.push(hyperbridge.into());
+		let mut parachains = parachains_paras::Parachains::<Runtime>::get()
+			.into_iter()
+			.collect::<alloc::collections::BTreeSet>();
+		parachains.insert(hyperbridge.into());
 		let mut para_heads: Vec<(u32, Vec<u8>)> = parachains
 			.into_iter()
 			.filter_map(|id| {
