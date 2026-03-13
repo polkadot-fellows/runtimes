87a49aec145cb101bce5cf8689b618986020087a
Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
2026-03-13 16:12:11 +0200

    Bump spec versions to 2.1.1
    Signed-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>

--- system-parachains/asset-hubs/asset-hub-kusama/src/lib.rs
+++ system-parachains/asset-hubs/asset-hub-kusama/src/lib.rs
@@ -150,7 +150,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: Cow::Borrowed("statemine"),
 	impl_name: Cow::Borrowed("statemine"),
 	authoring_version: 1,
-	spec_version: 2_001_000,
+	spec_version: 2_001_001,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 15,
