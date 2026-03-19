7289a47fd36b6b7302d89ffc91fd3518c02e9826
Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
2026-03-19 20:44:23 +0200

    transfer sufficient assets out of closed bounty accs
    Signed-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>

--- system-parachains/asset-hubs/asset-hub-polkadot/src/treasury.rs
+++ system-parachains/asset-hubs/asset-hub-polkadot/src/treasury.rs
@@ -17,6 +17,7 @@ use crate::{governance::Treasurer, *};
 use frame_support::traits::{
 	fungible::HoldConsideration, tokens::UnityOrOuterConversion, FromContains,
 };
+use pallet_bounties::TransferAllFungibles;
 use parachains_common::pay::{AccountIdToLocalLocation, LocalPay, VersionedLocatableAccount};
 use polkadot_runtime_common::impls::{ContainsParts, VersionedLocatableAsset};
 
@@ -63,6 +64,22 @@ impl pallet_treasury::Config for Runtime {
 
 parameter_types! {
 	// where `176` is the size of the `Bounty` struct in bytes.
+	// Assets that legacy bounties can hold: native DOT, USDT (1984), USDC (1337).
+	pub BountyRelevantAssets: Vec<xcm::latest::Location> = vec![
+		xcm_config::DotLocation::get(),
+		xcm::latest::Location::new(
+			0,
+			[xcm::latest::Junction::PalletInstance(
+				xcm_config::TrustBackedAssetsPalletIndex::get(),
+			), xcm::latest::Junction::GeneralIndex(1984)],
+		),
+		xcm::latest::Location::new(
+			0,
+			[xcm::latest::Junction::PalletInstance(
+				xcm_config::TrustBackedAssetsPalletIndex::get(),
+			), xcm::latest::Junction::GeneralIndex(1337)],
+		),
+	];
 	pub const BountyDepositBase: Balance = system_para_deposit(0, 176);
 	// per byte for the bounty description.
 	pub const DataDepositPerByte: Balance = system_para_deposit(0, 1);
@@ -90,7 +107,8 @@ impl pallet_bounties::Config for Runtime {
 	type MaximumReasonLength = MaximumReasonLength;
 	type OnSlash = Treasury;
 	type WeightInfo = weights::pallet_bounties::WeightInfo<Runtime>;
-	type TransferAllAssets = (); // TODO @ggwpez
+	type TransferAllAssets =
+		TransferAllFungibles<AccountId, NativeAndAssets, BountyRelevantAssets>;
 }
 
 parameter_types! {
