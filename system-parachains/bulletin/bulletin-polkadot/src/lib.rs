fcf79aca35a1b05d34595112f4107b51ac0611a7
Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
2026-04-16 12:03:04 +0200

    Bump spec to 221
    Signed-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>

--- system-parachains/bulletin/bulletin-polkadot/src/lib.rs
+++ system-parachains/bulletin/bulletin-polkadot/src/lib.rs
@@ -150,7 +150,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: alloc::borrow::Cow::Borrowed("bulletin-polkadot"),
 	impl_name: alloc::borrow::Cow::Borrowed("bulletin-polkadot"),
 	authoring_version: 1,
-	spec_version: 2_002_000,
+	spec_version: 2_002_001,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
