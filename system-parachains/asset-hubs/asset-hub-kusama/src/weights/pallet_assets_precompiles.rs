6c449910450136cf9bfad766a182de526ed06a96
Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
2026-04-24 19:09:44 +0200

    Integrate https://github.com/paritytech/polkadot-sdk/pull/11279
    Signed-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>

--- system-parachains/asset-hubs/asset-hub-kusama/src/weights/pallet_assets_precompiles.rs
+++ system-parachains/asset-hubs/asset-hub-kusama/src/weights/pallet_assets_precompiles.rs
@@ -102,7 +102,7 @@ impl<T: frame_system::Config> pallet_assets_precompiles::WeightInfo for WeightIn
 	/// Proof: `ForeignAssets::Approvals` (`max_values`: None, `max_size`: Some(746), added: 3221, mode: `MaxEncodedLen`)
 	/// Storage: `System::Account` (r:1 w:1)
 	/// Proof: `System::Account` (`max_values`: None, `max_size`: Some(128), added: 2603, mode: `MaxEncodedLen`)
-	fn permit() -> Weight {
+	fn use_permit() -> Weight {
 		// Proof Size summary in bytes:
 		//  Measured:  `2409`
 		//  Estimated: `6044`
