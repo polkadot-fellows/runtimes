d00009843e7608795a8e7518b7d4e608d818a33d
Mónica Jin <monica@parity.io>
2026-05-06 15:30:48 +0200

    bump transaction_version

--- system-parachains/asset-hubs/asset-hub-polkadot/src/lib.rs
+++ system-parachains/asset-hubs/asset-hub-polkadot/src/lib.rs
@@ -204,7 +204,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_version: 2_002_002,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
-	transaction_version: 15,
+	transaction_version: 16,
 	system_version: 1,
 };
 
