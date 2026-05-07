d2df2277f85a72dd9eba4373d84f2ed645ace8c9
Ankan <ankan.anurag@gmail.com>
2026-05-07 09:52:35 +0200

    wah changes

--- system-parachains/asset-hubs/asset-hub-polkadot/src/weights/mod.rs
+++ system-parachains/asset-hubs/asset-hub-polkadot/src/weights/mod.rs
@@ -35,6 +35,8 @@ pub mod pallet_bounties;
 pub mod pallet_child_bounties;
 pub mod pallet_collator_selection;
 pub mod pallet_conviction_voting;
+
+pub mod pallet_dap;
 pub mod pallet_election_provider_multi_block;
 pub mod pallet_election_provider_multi_block_signed;
 pub mod pallet_election_provider_multi_block_unsigned;
