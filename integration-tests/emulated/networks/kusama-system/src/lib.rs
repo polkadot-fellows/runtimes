776106a7a4575c4a84ecb2f2c6b72e055f344e8c
Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
2026-05-22 13:56:16 +0300

    disable encointer
    Signed-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>

--- integration-tests/emulated/networks/kusama-system/src/lib.rs
+++ integration-tests/emulated/networks/kusama-system/src/lib.rs
@@ -16,7 +16,7 @@
 pub use asset_hub_kusama_emulated_chain;
 pub use bridge_hub_kusama_emulated_chain;
 pub use coretime_kusama_emulated_chain;
-pub use encointer_kusama_emulated_chain;
+// TODO @ggwpez pub use encointer_kusama_emulated_chain;
 pub use kusama_emulated_chain;
 pub use penpal_emulated_chain;
 pub use people_kusama_emulated_chain;
@@ -24,7 +24,7 @@ pub use people_kusama_emulated_chain;
 use asset_hub_kusama_emulated_chain::AssetHubKusama;
 use bridge_hub_kusama_emulated_chain::BridgeHubKusama;
 use coretime_kusama_emulated_chain::CoretimeKusama;
-use encointer_kusama_emulated_chain::EncointerKusama;
+// TODO @ggwpez use encointer_kusama_emulated_chain::EncointerKusama;
 use kusama_emulated_chain::Kusama;
 use penpal_emulated_chain::{PenpalA, PenpalB};
 use people_kusama_emulated_chain::PeopleKusama;
@@ -45,7 +45,7 @@ decl_test_networks! {
 			PenpalB,
 			PeopleKusama,
 			CoretimeKusama,
-			EncointerKusama,
+			// TODO @ggwpez EncointerKusama,
 		],
 		bridge = ()
 	},
@@ -58,6 +58,6 @@ decl_test_sender_receiver_accounts_parameter_types! {
 	PenpalAPara { sender: ALICE, receiver: BOB },
 	PenpalBPara { sender: ALICE, receiver: BOB },
 	PeopleKusamaPara { sender: ALICE, receiver: BOB },
-	CoretimeKusamaPara { sender: ALICE, receiver: BOB },
-	EncointerKusamaPara { sender: ALICE, receiver: BOB }
+	CoretimeKusamaPara { sender: ALICE, receiver: BOB }
+	// TODO @ggwpez EncointerKusamaPara { sender: ALICE, receiver: BOB }
 }
