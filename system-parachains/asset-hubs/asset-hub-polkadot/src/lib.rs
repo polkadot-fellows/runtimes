02e17275700b43284567efcc4cbae087f9affff3
theo <37565691+agustinustheo@users.noreply.github.com>
2026-08-26 13:51:11 +0700

    Update system-parachains/asset-hubs/asset-hub-polkadot/src/lib.rs
    Co-authored-by: Guillaume Thiolliere <guillaume.thiolliere@parity.io>

--- system-parachains/asset-hubs/asset-hub-polkadot/src/lib.rs
+++ system-parachains/asset-hubs/asset-hub-polkadot/src/lib.rs
@@ -205,7 +205,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	impl_name: Cow::Borrowed("statemint"),
 	spec_name: Cow::Borrowed("statemint"),
 	authoring_version: 1,
-	spec_version: 2_003_003,
+	spec_version: 2_003_002,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 15,
