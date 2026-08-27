c632e472081ca772fd36b7fb10bff7aaafc88bea
Andrzej Sulkowski <developer@andrzej-sulkowski.com>
2026-08-27 16:10:27 +0200

    Merge pull request #7 from andrzejSulkowski/andrzej-integration-tests
    Emulated Tests

--- integration-tests/emulated/tests/people/people-polkadot/src/tests/individuality.rs
+++ integration-tests/emulated/tests/people/people-polkadot/src/tests/individuality.rs
@@ -1,15 +1,14 @@
 // Copyright (C) Parity Technologies (UK) Ltd.
 // SPDX-License-Identifier: Apache-2.0
 
-//! Tests the People-to-Asset-Hub Individuality ring-root notification path.
-
 use crate::*;
+use asset_hub_polkadot_runtime::governance::pallet_custom_origins::Origin;
 use cumulus_pallet_parachain_system::{
 	relay_state_snapshot::MessagingStateSnapshot, RelevantMessagingState,
 };
-use frame_support::BoundedVec;
+use frame_support::{assert_noop, sp_runtime::traits::Dispatchable, BoundedVec};
 use indiv_pallet_members::{CurrentRingIndex, Root};
-use indiv_pallet_members_notifier::{PendingInit, Subscribers};
+use indiv_pallet_members_notifier::{PendingInit, Subscribers, SubscriptionWhitelist};
 use indiv_pallet_members_subscriber::{
 	Pallet as MembersSubscriber, RingCollectionExponents, Subscription,
 };
@@ -101,6 +100,131 @@ fn people_ring_root_notification_activates_asset_hub_subscriber() {
 		assert_eq!(
 			asset_hub_polkadot_runtime::individuality::PgasAssetId::get(),
 			asset_hub_polkadot_runtime::individuality::PGAS_ASSET_ID,
+		);
+	});
+}
+
+fn send_asset_hub_transact_to_people(
+	asset_hub_origin: <AssetHubPolkadot as Chain>::RuntimeOrigin,
+	people_origin_kind: OriginKind,
+	people_call: &<PeoplePolkadot as Chain>::RuntimeCall,
+) -> DispatchResult {
+	type AssetHubRuntime = <AssetHubPolkadot as Chain>::Runtime;
+	type AssetHubRuntimeCall = <AssetHubPolkadot as Chain>::RuntimeCall;
+
+	AssetHubPolkadot::execute_with(|| {
+		let send = AssetHubRuntimeCall::PolkadotXcm(pallet_xcm::Call::<AssetHubRuntime>::send {
+			dest: bx!(VersionedLocation::from(AssetHubPolkadot::sibling_location_of(
+				PeoplePolkadot::para_id()
+			))),
+			message: bx!(VersionedXcm::from(Xcm(vec![
+				UnpaidExecution { weight_limit: Unlimited, check_origin: None },
+				Transact {
+					origin_kind: people_origin_kind,
+					fallback_max_weight: None,
+					call: people_call.encode().into(),
+				},
+			]))),
+		});
+		send.dispatch(asset_hub_origin).map(|_| ()).map_err(|e| e.error)
+	})
+}
+
+#[test]
+fn asset_hub_governance_creates_a_coinage_instance() {
+	type PeopleRuntime = <PeoplePolkadot as Chain>::Runtime;
+	type PeopleRuntimeCall = <PeoplePolkadot as Chain>::RuntimeCall;
+	type PeopleRuntimeEvent = <PeoplePolkadot as Chain>::RuntimeEvent;
+	type PeopleRuntimeOrigin = <PeoplePolkadot as Chain>::RuntimeOrigin;
+	type AssetHubRuntimeOrigin = <AssetHubPolkadot as Chain>::RuntimeOrigin;
+
+	let asset = HollarLocation::get();
+	let pallet_account = people_polkadot_runtime::Coinage::pallet_account();
+
+	let create_asset =
+		PeopleRuntimeCall::Assets(pallet_assets::Call::<PeopleRuntime>::force_create {
+			id: asset.clone(),
+			owner: pallet_account.clone().into(),
+			is_sufficient: true,
+			min_balance: 1,
+		});
+	let create_instance = PeopleRuntimeCall::Coinage(
+		indiv_pallet_coinage::Call::<PeopleRuntime>::create_sufficient_instance {
+			asset_id: asset.clone(),
+			asset_unit: HOLLAR_UNITS / 100,
+		},
+	);
+
+	assert_ok!(send_asset_hub_transact_to_people(
+		AssetHubRuntimeOrigin::root(),
+		OriginKind::Superuser,
+		&create_asset,
+	));
+
+	PeoplePolkadot::execute_with(|| {
+		PeoplePolkadot::assert_xcmp_queue_success(None);
+		assert_expected_events!(
+			PeoplePolkadot,
+			vec![
+				PeopleRuntimeEvent::Assets(pallet_assets::Event::ForceCreated { .. }) => {},
+			]
+		);
+
+		// Emulate the reserve-transferred HOLLAR buffer required before instance creation.
+		assert_ok!(people_polkadot_runtime::Assets::mint(
+			PeopleRuntimeOrigin::signed(pallet_account.clone()),
+			asset.clone(),
+			pallet_account.clone().into(),
+			1,
+		));
+	});
+
+	assert_ok!(send_asset_hub_transact_to_people(
+		Origin::StakingAdmin.into(),
+		OriginKind::Xcm,
+		&create_instance,
+	));
+	PeoplePolkadot::execute_with(|| {
+		PeoplePolkadot::assert_xcmp_queue_success(None);
+		assert!(indiv_pallet_coinage::Instances::<PeopleRuntime>::get(0).is_none());
+	});
+
+	assert_ok!(send_asset_hub_transact_to_people(
+		Origin::TechnicalMaintenance.into(),
+		OriginKind::Xcm,
+		&create_instance,
+	));
+
+	PeoplePolkadot::execute_with(|| {
+		PeoplePolkadot::assert_xcmp_queue_success(None);
+		let instance = indiv_pallet_coinage::Instances::<PeopleRuntime>::get(0)
+			.expect("technical_maintenance created the instance");
+		assert_eq!(instance.asset_id, asset);
+		assert_eq!(instance.asset_unit, HOLLAR_UNITS / 100);
+		assert_eq!(instance.mode, indiv_pallet_coinage::InstanceMode::Sufficient);
+	});
+}
+
+#[test]
+fn asset_hub_subscribes_from_the_genesis_whitelist() {
+	type PeopleRuntime = <PeoplePolkadot as Chain>::Runtime;
+	type PeopleRuntimeOrigin = <PeoplePolkadot as Chain>::RuntimeOrigin;
+
+	PeoplePolkadot::execute_with(|| {
+		let para_id = AssetHubPolkadot::para_id();
+		let authorized = PeopleRuntimeOrigin::from(frame_system::RawOrigin::Authorized);
+
+		assert_ok!(people_polkadot_runtime::MembersNotifier::subscribe_whitelisted(
+			authorized.clone(),
+			para_id,
+		));
+
+		assert!(Subscribers::<PeopleRuntime>::contains_key(para_id));
+		assert!(PendingInit::<PeopleRuntime>::contains_key(para_id));
+		assert!(!SubscriptionWhitelist::<PeopleRuntime>::contains_key(para_id));
+		assert_noop!(
+			people_polkadot_runtime::MembersNotifier::subscribe_whitelisted(authorized, para_id),
+			indiv_pallet_members_notifier::Error::<PeopleRuntime>::NotWhitelisted
 		);
 	});
 }
