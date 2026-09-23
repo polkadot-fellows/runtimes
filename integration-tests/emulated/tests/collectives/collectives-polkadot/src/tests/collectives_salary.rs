6ec84138f1430377a25e9b8e1fce53534eba383e
jessechejieh <jessechejieh@gmail.com>
2026-09-23 23:22:12 +0100

    integration testing

--- integration-tests/emulated/tests/collectives/collectives-polkadot/src/tests/collectives_salary.rs
+++ integration-tests/emulated/tests/collectives/collectives-polkadot/src/tests/collectives_salary.rs
@@ -16,10 +16,12 @@
 use crate::*;
 use asset_hub_polkadot_runtime::xcm_config::LocationToAccountId;
 use collectives_polkadot_runtime::fellowship::FellowshipSalaryPaymaster;
+use emulated_integration_tests_common::accounts::BOB;
 use frame_support::{
 	assert_ok,
 	traits::{fungibles::Mutate, tokens::Pay},
 };
+use parachains_common::AccountId;
 use xcm_executor::traits::ConvertLocation;
 
 const FELLOWSHIP_SALARY_PALLET_ID: u8 =
@@ -108,5 +110,115 @@ fn pay_salary_secretary() {
 				RuntimeEvent::MessageQueue(pallet_message_queue::Event::Processed { success: true ,.. }) => {},
 			]
 		);
+	});
+}
+
+const SALARY: u128 = 1_000_000_000;
+
+/// Makes `fellow` a rank 1 Fellow with `SALARY`, gives `delegate` a `NonTransfer` proxy for it and
+/// moves the salary pallet into the payout window of a fresh cycle.
+fn fellow_in_payout_window_with_non_transfer_delegate(fellow: &AccountId, delegate: &AccountId) {
+	use collectives_polkadot_runtime::{
+		fellowship::FellowshipCoreInstance, FellowshipCollective, FellowshipCore, FellowshipSalary,
+		Proxy, ProxyType, Runtime, RuntimeOrigin,
+	};
+	use frame_support::traits::RankedMembers;
+	use polkadot_runtime_constants::time::DAYS;
+
+	assert_ok!(<FellowshipCollective as RankedMembers>::induct(fellow));
+	assert_ok!(<FellowshipCollective as RankedMembers>::promote(fellow));
+	pallet_core_fellowship::Params::<Runtime, FellowshipCoreInstance>::mutate(|p| {
+		p.active_salary = vec![SALARY].try_into().unwrap();
+		p.passive_salary = vec![SALARY].try_into().unwrap();
+	});
+	assert_ok!(FellowshipCore::import_member(
+		RuntimeOrigin::signed(fellow.clone()),
+		fellow.clone()
+	));
+
+	let start = frame_system::Pallet::<Runtime>::block_number();
+	assert_ok!(FellowshipSalary::init(RuntimeOrigin::signed(fellow.clone())));
+	assert_ok!(FellowshipSalary::induct(RuntimeOrigin::signed(fellow.clone())));
+	// One cycle is 15 days of registration plus 15 days of payout.
+	frame_system::Pallet::<Runtime>::set_block_number(start + 30 * DAYS);
+	assert_ok!(FellowshipSalary::bump(RuntimeOrigin::signed(fellow.clone())));
+	frame_system::Pallet::<Runtime>::set_block_number(start + 45 * DAYS);
+
+	assert_ok!(Proxy::add_proxy(
+		RuntimeOrigin::signed(fellow.clone()),
+		delegate.clone().into(),
+		ProxyType::NonTransfer,
+		0
+	));
+}
+
+#[test]
+fn non_transfer_proxy_cannot_redirect_fellowship_salary() {
+	use collectives_polkadot_runtime::{FellowshipSalary, Proxy, RuntimeCall, RuntimeOrigin};
+	use frame_support::traits::fungibles::Inspect;
+
+	const USDT_ID: u32 = 1984;
+	let fellowship_salary = (
+		Parent,
+		Parachain(CollectivesPolkadot::para_id().into()),
+		PalletInstance(FELLOWSHIP_SALARY_PALLET_ID),
+	);
+	let pay_from = LocationToAccountId::convert_location(&fellowship_salary.into()).unwrap();
+	let fellow = CollectivesPolkadot::account_id_of(ALICE);
+	let delegate = CollectivesPolkadot::account_id_of(BOB);
+
+	AssetHubPolkadot::execute_with(|| {
+		type AssetHubAssets = <AssetHubPolkadot as AssetHubPolkadotPallet>::Assets;
+		assert_ok!(<AssetHubAssets as Mutate<_>>::mint_into(USDT_ID, &pay_from, SALARY * 2));
+		assert_eq!(<AssetHubAssets as Inspect<_>>::balance(USDT_ID, &delegate), 0);
+	});
+
+	CollectivesPolkadot::execute_with(|| {
+		type RuntimeEvent = <CollectivesPolkadot as Chain>::RuntimeEvent;
+		fellow_in_payout_window_with_non_transfer_delegate(&fellow, &delegate);
+
+		// The delegate tries to send the Fellow's salary to itself.
+		assert_ok!(Proxy::proxy(
+			RuntimeOrigin::signed(delegate.clone()),
+			fellow.clone().into(),
+			None,
+			Box::new(RuntimeCall::FellowshipSalary(pallet_salary::Call::payout_other {
+				beneficiary: delegate.clone(),
+			})),
+		));
+		assert_expected_events!(
+			CollectivesPolkadot,
+			vec![
+				RuntimeEvent::Proxy(pallet_proxy::Event::ProxyExecuted { result }) => {
+					result: *result == Err(frame_system::Error::<
+						collectives_polkadot_runtime::Runtime,
+					>::CallFiltered
+						.into()),
+				},
+			]
+		);
+
+		// The same call made by the Fellow itself does pay the delegate.
+		assert_ok!(FellowshipSalary::payout_other(
+			RuntimeOrigin::signed(fellow.clone()),
+			delegate.clone()
+		));
+		assert_expected_events!(
+			CollectivesPolkadot,
+			vec![
+				RuntimeEvent::FellowshipSalary(pallet_salary::Event::Paid { who, beneficiary, amount, .. }) => {
+					who: *who == fellow,
+					beneficiary: *beneficiary == delegate,
+					amount: *amount == SALARY,
+				},
+				RuntimeEvent::XcmpQueue(cumulus_pallet_xcmp_queue::Event::XcmpMessageSent { .. }) => {},
+			]
+		);
+	});
+
+	AssetHubPolkadot::execute_with(|| {
+		type AssetHubAssets = <AssetHubPolkadot as AssetHubPolkadotPallet>::Assets;
+		// Only the Fellow's own call paid out.
+		assert_eq!(<AssetHubAssets as Inspect<_>>::balance(USDT_ID, &delegate), SALARY);
 	});
 }
